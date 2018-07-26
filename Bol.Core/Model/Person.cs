ff40471146094742913347fe94a55978948b8d9f
Charalampos Chomenidis <hampos@gmail.com>
2018-07-27 02:51:43 +0300

    Add MIddleName and Combination properties on Person

--- Bol.Core/Model/Person.cs
+++ Bol.Core/Model/Person.cs
@@ -8,13 +8,16 @@ namespace Bol.Core.Model
         public string Country { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
+        public string MiddleName { get; set; }
         public Gender Gender { get; set; }
         public DateTime Birthdate { get; set; }
+        public string Combination { get; set; }
     }
 
     public enum Gender
     {
         Male,
-        Female
+        Female,
+        Unspecified
     }
 }
