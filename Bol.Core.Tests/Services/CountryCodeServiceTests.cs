43dacb9500466b3cf74c00de7497659571ebc8f4
Charalampos Chomenidis <hampos@gmail.com>
2018-08-01 23:28:02 +0300

    Rename tests and split assertions to single per test

--- Bol.Core.Tests/Services/CountryCodeServiceTests.cs
+++ Bol.Core.Tests/Services/CountryCodeServiceTests.cs
@@ -1,49 +1,73 @@
-﻿using Bol.Core.Serializers;
+﻿using Bol.Core.Model;
 using Bol.Core.Services;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace Bol.Core.Tests.Services
 {
     public class CountryCodeServiceTests
     {
-        [Fact]
-        public void CheckCountryCodeDictionary()
+        private readonly CountryCodeService _service;
+
+        public CountryCodeServiceTests()
         {
-            var service = new CountryCodeService(new CountriesJsonSerializer());
-            service.GetCountryCodes().TryGetValue("Greece", out string value);
-            Assert.Equal("GRC", value);
+            var section = File.ReadAllText(AppContext.BaseDirectory + ".content/country_code.json");
+            var countries = JsonConvert.DeserializeObject<List<Country>>(section);
+            var options = Options.Create(countries);
+            _service = new CountryCodeService(options);
         }
 
         [Fact]
-        public void ShouldBeCountryCode()
+        public void GetCode_ShouldReturnCountryCode_WhenCountryWithNameExists()
         {
-            var service = new CountryCodeService(new CountriesJsonSerializer());
-            Assert.True(service.IsValidCode("GRC"));
-            Assert.False(service.IsValidCode("GRT"));
+            var code = _service.GetCode("Greece");
+            Assert.Equal("GRC", code);
         }
 
         [Fact]
-        public void ShouldBeCountryName()
+        public void GetCode_ShouldReturnNull_WhenCountryWithNameNotExists()
         {
-            var service = new CountryCodeService(new CountriesJsonSerializer());
-            Assert.True(service.IsValidCountry("Greece"));
-            Assert.False(service.IsValidCountry("Greete"));
+            Assert.Null(_service.GetCountry("Greete"));
         }
 
         [Fact]
-        public void CountrysNameShouldBe()
+        public void IsValidCode_ShouldReturnTrue_WhenCountryCodeExists()
         {
-            var service = new CountryCodeService(new CountriesJsonSerializer());
-            Assert.Equal("Greece", service.GetCountry("GRC"));
-            Assert.Null(service.GetCountry("GRT"));
+            Assert.True(_service.IsValidCode("GRC"));
         }
 
         [Fact]
-        public void CountrysCodeShouldBe()
+        public void IsValidCode_ShouldReturnFalse_WhenCountryCodeNotExists()
         {
-            var service = new CountryCodeService(new CountriesJsonSerializer());
-            Assert.Equal("GRC", service.GetCode("Greece"));
-            Assert.Null(service.GetCountry("Greete"));
+            Assert.False(_service.IsValidCode("GRT"));
+        }
+
+        [Fact]
+        public void IsValidCountry_ShouldReturnTrue_WhenCountryWithNameExists()
+        {
+            Assert.True(_service.IsValidCountry("Greece"));
+        }
+
+        [Fact]
+        public void IsValidCountry_ShouldReturnFalse_WhenCountryWithNameNotExists()
+        {
+            Assert.False(_service.IsValidCountry("Greete"));
+        }
+
+        [Fact]
+        public void GetCountry_ShouldReturnCountry_WhenCodeExists()
+        {
+            Assert.Equal("Greece", _service.GetCountry("GRC").Name);
+        }
+
+        [Fact]
+        public void GetCountry_ShouldReturnNull_WhenCodeNotExists()
+        {
+            Assert.Null(_service.GetCountry("GRT"));
         }
     }
 }
