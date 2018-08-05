ee0d391a52545e2578f7dee14e487e64db680ba2
Charalampos Chomenidis <hampos@gmail.com>
2018-08-05 20:20:09 +0300

    Change Person serialization to include Combination and MiddleName

--- Bol.Core/Serializers/PersonStringSerializer.cs
+++ Bol.Core/Serializers/PersonStringSerializer.cs
@@ -18,7 +18,7 @@ namespace Bol.Core.Serializers
             string birthYear = person.Birthdate.Year.ToString();
             birthYear = birthYear.Substring(birthYear.Length - 2);
 
-            return $"P{DIV}{person.Country}{DIV}{person.Surname}{DIV}{person.Name}{DIV}{birthYear}{gender}{DIV}";
+            return $"P{DIV}{person.CountryCode}{DIV}{person.Surname}{DIV}{person.Name}{DIV}{person.MiddleName}{DIV}{birthYear}{gender}{DIV}";
         }
     }
 }
