35cffb51c84cf08b93c38733f7735e8ff20eb872
Charalampos Chomenidis <hampos@gmail.com>
2018-08-05 20:18:19 +0300

    Add min/max combination length, has all lettters capital, and country code exists validations

--- Bol.Core/Validators/PersonValidator.cs
+++ Bol.Core/Validators/PersonValidator.cs
@@ -1,32 +1,56 @@
-﻿using Bol.Core.Model;
+﻿using Bol.Core.Abstractions;
+using Bol.Core.Model;
 using FluentValidation;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Bol.Core.Validators
 {
     public class PersonValidator : AbstractValidator<Person>
     {
-        public PersonValidator()
+        private readonly ICountryCodeService _countryCodeService;
+        private readonly Regex _capitalLetters = new Regex(@"^[A-Z]+$");
+
+        public PersonValidator(ICountryCodeService countryCodeService)
         {
+            _countryCodeService = countryCodeService ?? throw new ArgumentNullException(nameof(countryCodeService));
+
             RuleFor(p => p).NotEmpty().WithMessage("Person object cannot be empty.");
             RuleFor(p => p.Nin).NotEmpty().WithMessage("National Identification Number cannot be empty.");
             RuleFor(p => p.Name).NotEmpty().WithMessage("Name cannot be empty.");
             RuleFor(p => p.Surname).NotEmpty().WithMessage("Surname cannot be empty.");
             RuleFor(p => p.Combination).NotEmpty().WithMessage("2 digit combination cannot be empty.");
-            RuleFor(p => p.Country).NotEmpty().WithMessage("Country cannot be empty.");
+            RuleFor(p => p.CountryCode).NotEmpty().WithMessage("Country cannot be empty.");
 
-            RuleFor(p => p.Name).Must(HasCapitalLetters).WithMessage("Name must consist of capital letters A-Z.");
-            RuleFor(p => p.Surname).Must(HasCapitalLetters).WithMessage("Surname must consist of capital letters A-Z.");
+            RuleFor(p => p.Name).Must(HasAllLettersCapital).WithMessage("Name must consist of capital letters A-Z.");
+            RuleFor(p => p.Surname).Must(HasAllLettersCapital).WithMessage("Surname must consist of capital letters A-Z.");
             RuleFor(p => p.MiddleName)
-                .Must(HasCapitalLetters)
+                .Must(HasAllLettersCapital)
                 .When(p => !string.IsNullOrEmpty(p.MiddleName))
                 .WithMessage("Middle Name must consist of capital letters A-Z.");
 
+            RuleFor(p => p.Combination)
+                .MinimumLength(2)
+                .WithMessage("Combination cannot have less than 2 digits.");
 
+            RuleFor(p => p.Combination)
+                .MaximumLength(2)
+                .WithMessage("Combination cannot have more than 2 digits.");
+
+            RuleFor(p => p.CountryCode)
+                .Must(CountryCodeExists)
+                .WithMessage("Country Code is not valid.");
         }
 
-        private bool HasCapitalLetters(string input)
+        private bool HasAllLettersCapital(string input)
         {
-            return true;
+            return _capitalLetters.IsMatch(input);
+        }
+
+        private bool CountryCodeExists(string code)
+        {
+            return _countryCodeService.IsValidCode(code);
         }
     }
 }
