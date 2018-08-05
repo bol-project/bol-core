cedc2830b0bf100731d5cb66f3cf6937e9301c62
Charalampos Chomenidis <hampos@gmail.com>
2018-08-05 20:17:14 +0300

    Add IEnumerableExtension SkipLastN

---
+++ Bol.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> SkipLastN<T>(this IEnumerable<T> source, int n)
+        {
+            var it = source.GetEnumerator();
+            bool hasRemainingItems = false;
+            var cache = new Queue<T>(n + 1);
+
+            do
+            {
+                if (hasRemainingItems = it.MoveNext())
+                {
+                    cache.Enqueue(it.Current);
+                    if (cache.Count > n)
+                        yield return cache.Dequeue();
+                }
+            } while (hasRemainingItems);
+        }
+    }
+}
