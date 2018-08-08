3e43b7aebc93a323e390fcf08993d09b8609144d
Charalampos Chomenidis <hampos@gmail.com>
2018-08-08 16:36:43 +0300

    Add CodeNameServiceValidated decorator that validates person before generating CodeName

---
+++ Bol.Core/Services/Decorators/CodeNameServiceValidated.cs
@@ -0,0 +1,25 @@
+﻿using Bol.Core.Abstractions;
+using Bol.Core.Model;
+using FluentValidation;
+using System;
+
+namespace Bol.Core.Services.Decorators
+{
+    public class CodeNameServiceValidated : ICodeNameService
+    {
+        private readonly ICodeNameService _service;
+        private readonly IValidator<Person> _validator;
+
+        public CodeNameServiceValidated(ICodeNameService service, IValidator<Person> validator)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public string Generate(Person person)
+        {
+            _validator.ValidateAndThrow(person);
+            return _service.Generate(person);
+        }
+    }
+}
