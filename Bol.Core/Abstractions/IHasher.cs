02ef8f55b596f878379a6bebe8179ded970f3962
Charalampos Chomenidis <hampos@gmail.com>
2018-08-08 16:35:45 +0300

    Add optional cycles and bytes/characters parameters on IHasher AddChecksum and CheckChecksum methods

--- Bol.Core/Abstractions/IHasher.cs
+++ Bol.Core/Abstractions/IHasher.cs
@@ -5,10 +5,10 @@
         string Hash(string input);
         byte[] Hash(byte[] input);
 
-        byte[] AddChecksum(byte[] input);
-        string AddChecksum(string input);
+        byte[] AddChecksum(byte[] input, uint cycles = 1, uint bytes = 2);
+        string AddChecksum(string input, uint cycles = 1, uint characters = 4);
 
-        bool CheckChecksum(byte[] input);
-        bool CheckChecksum(string input);
+        bool CheckChecksum(byte[] input, uint cycles = 1, uint bytes = 2);
+        bool CheckChecksum(string input, uint cycles = 1, uint characters = 4);
     }
 }
--- Bol.Core/Hashers/Sha256Hasher.cs
+++ Bol.Core/Hashers/Sha256Hasher.cs
@@ -18,29 +18,52 @@ namespace Bol.Core.Hashers
             _encoder = encoder ?? throw new ArgumentNullException(nameof(encoder));
         }
 
-        public byte[] AddChecksum(byte[] input)
+        public byte[] AddChecksum(byte[] input, uint cycles = 1, uint bytes = 2)
         {
+            if (cycles < 1) throw new ArgumentException("At least 1 Hashing cycle is needed for the checksum.");
+            if (bytes <= 0) throw new ArgumentException("At least 1 byte of the Hash is needed for the checksum.");
+
             var hash = Hash(input);
-            hash = Hash(hash);
-            return input.Concat(hash.Take(4)).ToArray();
+            for (int i = 1; i < cycles; i++)
+            {
+                hash = Hash(hash);
+            }
+            return input
+                .Concat(hash.Take(Convert.ToInt32(bytes)))
+                .ToArray();
         }
 
-        public string AddChecksum(string input)
+        public string AddChecksum(string input, uint cycles = 1, uint characters = 4)
         {
-            var hash = AddChecksum(Encoding.UTF8.GetBytes(input));
-            return _encoder.Encode(hash);
+            if (!(characters % 2 == 0)) throw new ArgumentException("Number of Hex Characters in the Checksum must be an even number.");
+
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hash = AddChecksum(inputBytes, cycles, characters / 2);
+            var checksum = _encoder.Encode(hash.Skip(inputBytes.Length).ToArray());
+
+            return input + checksum;
         }
 
-        public bool CheckChecksum(byte[] input)
+        public bool CheckChecksum(byte[] input, uint cycles = 1, uint bytes = 2)
         {
-            var inputWithoutChecksum = input.SkipLastN(4).ToArray();
-            var hash = AddChecksum(inputWithoutChecksum);
+            var inputWithoutChecksum = input
+                .SkipLastN(Convert.ToInt32(bytes))
+                .ToArray();
+
+            var hash = AddChecksum(inputWithoutChecksum, cycles, bytes);
             return hash.SequenceEqual(input);
         }
 
-        public bool CheckChecksum(string input)
+        public bool CheckChecksum(string input, uint cycles = 1, uint characters = 4)
         {
-            return CheckChecksum(Encoding.UTF8.GetBytes(input));
+            if (!(characters % 2 == 0)) throw new ArgumentException("Number of Hex Characters in the Checksum must be an even number.");
+
+            var inputWithoutChecksum = new string(input
+                .SkipLastN(Convert.ToInt32(characters))
+                .ToArray());
+
+            var hash = AddChecksum(inputWithoutChecksum, cycles, characters);
+            return hash.SequenceEqual(input);
         }
 
         public string Hash(string input)
