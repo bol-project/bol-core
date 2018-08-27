b904214424f68cd357027ef9d7fc3a360be3a914
Theofanis Tokas <tokastheofanis@gmail.com>
2018-08-27 21:58:43 +0300

    Add constants class

---
+++ Bol.Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Bol.Core
+{
+    public class Constants
+    {
+        public const char CODENAME_DIVIDER = '<';
+        public const string PERSONAL_CODENAME_INITIAL = "P";
+        public const string COMMERCIAL_CODENAME_INITIAL = "C";
+        public const string ORGANIZATION_CODENAME_INITIAL = "O";
+    }
+}
