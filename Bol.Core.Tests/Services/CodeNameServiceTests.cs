296da9466a51d83a4b901ef3087294ee07836e28
Charalampos Chomenidis <hampos@gmail.com>
2018-09-04 20:29:34 +0300

    Add PersonValidator unit tests for Name property

--- Bol.Core.Tests/Services/CodeNameServiceTests.cs
+++ Bol.Core.Tests/Services/CodeNameServiceTests.cs
@@ -3,7 +3,13 @@ using Bol.Core.Hashers;
 using Bol.Core.Model;
 using Bol.Core.Serializers;
 using Bol.Core.Services;
+using Bol.Core.Services.Decorators;
+using Bol.Core.Validators;
+using FluentValidation;
+using Microsoft.Extensions.Options;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Bol.Core.Tests.Services
@@ -13,20 +19,34 @@ namespace Bol.Core.Tests.Services
         [Fact]
         public void Generate_ShouldGenerateCodeName_WhenInputIsSpyros()
         {
-            var service = new CodeNameService(new PersonStringSerializer(), new PersonJsonSerializer(), new Sha256Hasher(new Base16Encoder()));
+            var hasher = new Sha256Hasher(new Base16Encoder());
+            var service = new CodeNameService(new PersonStringSerializer(), new PersonJsonSerializer(), hasher);
+            var countries = new List<Country> { new Country() { Name = "Greece", Alpha3 = "GRC" } };
+            var personValidator = new PersonValidator(new CountryCodeService(Options.Create(countries)));
+            var validatedService = new CodeNameServiceValidated(service, personValidator);
+            var codeNameValidator = new CodeNameValidator(personValidator, new PersonStringSerializer(), hasher);
+
+            var shortHash = hasher.Hash("A2347283423")
+                .Take(8)
+                .ToArray();
+            var shorthashString = new string(shortHash);
 
             var person = new Person
             {
-                Name = "Spyros",
-                Surname = "Pappas",
-                Country = "GRC",
+                Name = "SPYROS",
+                Surname = "PAPPAS",
+                CountryCode = "GRC",
                 Gender = Gender.Male,
-                Birthdate = DateTime.UtcNow,
-                Nin = "A2347283423"
+                Birthdate = DateTime.UtcNow - TimeSpan.FromDays(9192),
+                Nin = shorthashString,
+                Combination = "PP"
             };
 
-            var codeName = service.Generate(person, "PP");
-            Assert.Equal("P<GRC<Pappas<Spyros<18M<2BB6C323PPBEF3", codeName);
+            var codeName = validatedService.Generate(person);
+            Assert.Equal("P<GRC<PAPPAS<SPYROS<<93M<2BB6C323PP5D5D", codeName);
+            Assert.True(new Sha256Hasher(new Base16Encoder()).CheckChecksum("P<GRC<PAPPAS<SPYROS<<93M<2BB6C323PP5D5D"));
+
+            codeNameValidator.ValidateAndThrow("P<GRC<PAPPAS<SPYROS<<93M<2BB6C323PP5D5D");
         }
     }
 }
