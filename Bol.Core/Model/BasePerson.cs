5a34383e092a714df8fe3ba1610dc417f76aa3c3
Nikos Chomenidis <nchomenidis@workloud.com>
2018-10-09 17:01:58 +0300

    Create new BasePerson model that NaturalPerson and CodenamePerson inherit from. Create CodenamePerson model in order to model the deserialization of the codename string

---
+++ Bol.Core/Model/BasePerson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bol.Core.Model
+{
+	public class BasePerson
+	{
+		public string CountryCode { get; set; }
+		public string Surname { get; set; }
+		public string MiddleName { get; set; }
+		public string ThirdName { get; set; }
+		public Gender Gender { get; set; }
+		public string Combination { get; set; }
+	}
+
+	public enum Gender
+	{
+		Male,
+		Female,
+		Unspecified
+	}
+}
