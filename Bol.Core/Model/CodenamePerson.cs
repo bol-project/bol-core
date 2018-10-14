b365202bd1926fe8748df2a75724ccf58d45bbb9
Nikos Chomenidis <nhomenidis@me.com>
2018-10-15 01:47:54 +0300

    Change property from CodenamePerson model from DateTime to int

--- Bol.Core/Model/CodenamePerson.cs
+++ Bol.Core/Model/CodenamePerson.cs
@@ -7,7 +7,7 @@ namespace Bol.Core.Model
 	public class CodenamePerson : BasePerson 
 	{
 		public string FirstNameCharacter { get; set; }
-		public DateTime YearOfBirth { get; set; }
+		public int YearOfBirth { get; set; }
 		public string ShortHash { get; set; }
 		public string CheckSum { get; set; }
 	}
