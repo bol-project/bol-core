9d1b6148c24ac0e12ab451c2f25c3a0a97081a41
Charalampos Chomenidis <hampos@gmail.com>
2019-01-12 16:01:47 +0200

    Add NodeBackgroundService that starts neo-cli mainService

---
+++ Bol.Api/BackgroundServices/NodeBackgroundService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Neo.Shell;
+
+namespace Bol.Api.BackgroundServices
+{
+    public class NodeBackgroundService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {            
+            var mainService = new MainService();
+            mainService.Run(new string[0]);
+            
+            return Task.CompletedTask;
+        }
+    }
+}
