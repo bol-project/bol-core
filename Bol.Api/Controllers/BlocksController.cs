0e63dcbc63bcfe79e3300540ee85dcb9813045b9
Charalampos Chomenidis <hampos@gmail.com>
2019-01-12 16:01:24 +0200

    Add BlocksController

---
+++ Bol.Api/Controllers/BlocksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neo;
+using Neo.Ledger;
+using System.Linq;
+
+namespace Bol.Api.Controllers
+{
+    [Route("api/blocks")]
+    [ApiController]
+    public class BlocksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetBlocks()
+        {
+            var blocks = Blockchain.Singleton.Store.GetBlocks().Find();
+            var count = blocks.Count();
+            return Ok(blocks);
+        }
+
+        [HttpGet("current")]
+        public ActionResult GetCurrentBlock()
+        {
+            return Ok(Blockchain.Singleton.CurrentBlockHash.ToString());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            return Ok(Blockchain.Singleton.GetBlock(UInt256.Parse(id)).ToJson());
+        }
+    }
+}
