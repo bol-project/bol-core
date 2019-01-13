d455e98505e9a97d7a8f757a6df380316fe11a20
Charalampos Chomenidis <hampos@gmail.com>
2019-01-13 23:37:50 +0200

    Add Logging on  NodeBackgroundService exceptions

--- Bol.Api/BackgroundServices/NodeBackgroundService.cs
+++ Bol.Api/BackgroundServices/NodeBackgroundService.cs
@@ -1,19 +1,33 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Neo.Shell;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Neo.Shell;
 
 namespace Bol.Api.BackgroundServices
 {
     public class NodeBackgroundService : BackgroundService
     {
+        private readonly ILogger<NodeBackgroundService> _logger;
+
+        public NodeBackgroundService(ILogger<NodeBackgroundService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        {            
-            var mainService = new MainService();
-            mainService.Run(new string[0]);
-            
-            return Task.CompletedTask;
+        {
+            try
+            {
+                var mainService = new MainService();
+                return Task.Run(() => mainService.Run(new string[0]));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while executing MainService");
+                throw;
+            }
         }
     }
 }
