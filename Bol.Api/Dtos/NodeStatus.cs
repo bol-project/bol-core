d46fda787fcdea74150ad1b68dae3d7f84f4fe7c
Charalampos Chomenidis <hampos@gmail.com>
2019-01-15 00:24:11 +0200

    Add NodesController that exposes a status endpoint

---
+++ Bol.Api/Dtos/NodeStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Bol.Api.Dtos
+{
+    public class NodeStatus
+    {
+        public uint Block { get; set; }
+        public uint Height { get; set; }
+        public uint HeaderHeight { get; set; }
+        public int ConnectedNodes { get; set; }
+        public int DisconnectedNodes { get; set; }
+        public IEnumerable<NodeInfo> Nodes { get; set; }
+    }
+
+    public class NodeInfo
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        public int ListenPort { get; set; }
+        public uint? Height { get; set; }
+    }
+}
