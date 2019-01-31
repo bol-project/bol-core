213abb773d1b498598f8b335c1160a7a21f846d7
Charalampos Chomenidis <hampos@gmail.com>
2019-01-31 04:43:19 +0200

    Add BlockChainService wrapper

---
+++ Bol.Coin/Services/BlockChainService.cs
@@ -0,0 +1,13 @@
+﻿using Bol.Coin.Abstractions;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Bol.Coin.Services
+{
+    public class BlockChainService : IBlockChainService
+    {
+        public uint GetCurrentHeight()
+        {
+            return Blockchain.GetHeight();
+        }
+    }
+}
