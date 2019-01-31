e4437bcd296bdc48e06eccff155f6f2e0d34e86e
Charalampos Chomenidis <hampos@gmail.com>
2019-01-31 04:43:07 +0200

    Add RuntimeService wrapper

---
+++ Bol.Coin/Services/RuntimeService.cs
@@ -0,0 +1,13 @@
+﻿using Bol.Coin.Abstractions;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Bol.Coin.Services
+{
+    public class RuntimeService : IRuntimeService
+    {
+        public bool ValidateCallerAddress(byte[] address)
+        {
+            return Runtime.CheckWitness(address);
+        }
+    }
+}
