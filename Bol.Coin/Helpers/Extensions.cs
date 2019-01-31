b0c4fc5faa89f11854b3132c736acaa011161c30
Charalampos Chomenidis <hampos@gmail.com>
2019-01-31 04:46:08 +0200

    Add Extensions for easier conversion from/to byte arrays

---
+++ Bol.Coin/Helpers/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Bol.Coin
+{
+    public static class Extensions
+    {
+        public static double AsDouble(this byte[] source)
+        {
+            if (source == null) return 0;
+            if (source.Length == 0) return 0;
+
+            return BitConverter.ToDouble(source, 0);
+        }
+
+        public static int AsInt(this byte[] source)
+        {
+            if (source == null) return 0;
+            if (source.Length == 0) return 0;
+
+            return BitConverter.ToInt32(source, 0);
+        }
+
+        public static uint AsUInt(this byte[] source)
+        {
+            if (source == null) return 0;
+            if (source.Length == 0) return 0;
+
+            return BitConverter.ToUInt32(source, 0);
+        }
+
+        public static long AsLong(this byte[] source)
+        {
+            if (source == null) return 0;
+            if (source.Length == 0) return 0;
+
+            return BitConverter.ToInt64(source, 0);
+        }
+
+        public static bool AsBool(this byte[] source)
+        {
+            if (source == null) return false;
+            if (source.Length == 0) return false;
+
+            return BitConverter.ToBoolean(source, 0);
+        }
+    }
+}
