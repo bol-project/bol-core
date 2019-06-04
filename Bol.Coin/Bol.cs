d3d54c47cc1250d471cac4cd9232ab66905f21a0
Charalampos Chomenidis <hampos@gmail.com>
2019-06-04 20:01:32 +0300

    Serialize BolResponse to return byte arrays on all operations that use it

--- Bol.Coin/Bol.cs
+++ Bol.Coin/Bol.cs
@@ -11,83 +11,68 @@ namespace Neo.SmartContract
 
         public static Object Main(string operation, params object[] args)
         {
-            try
+            if (Runtime.Trigger == TriggerType.Verification)
             {
-                if (Runtime.Trigger == TriggerType.Verification)
+                return false; //Transferring from Contract account is forbidden
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "deploy") return BolService.Deploy();
+                if (operation == "totalSupply") return BolService.TotalSupply();
+                if (operation == "name") return BolService.Name();
+                if (operation == "symbol") return BolService.Symbol();
+                if (operation == "transfer")
                 {
-                    return false; //Transferring from Contract account is forbidden
+                    if (args.Length != 3) return false;
+                    byte[] from = (byte[])args[0];
+                    byte[] to = (byte[])args[1];
+                    BigInteger value = (BigInteger)args[2];
+                    //Runtime.Log(value.AsByteArray().AsString());
+                    return BolService.Transfer(from, to, value).Serialize();
                 }
-                else if (Runtime.Trigger == TriggerType.Application)
+                if (operation == "balanceOf")
                 {
-                    if (operation == "deploy") return BolService.Deploy();
-                    if (operation == "totalSupply") return BolService.TotalSupply();
-                    if (operation == "name") return BolService.Name();
-                    if (operation == "symbol") return BolService.Symbol();
-                    if (operation == "transfer")
-                    {
-                        if (args.Length != 3) return false;
-                        byte[] from = (byte[])args[0];
-                        byte[] to = (byte[])args[1];
-                        BigInteger value = (BigInteger)args[2];
-                        Runtime.Log(value.AsByteArray().AsString());
-                        return BolService.Transfer(from, to, value);
-                    }
-                    if (operation == "balanceOf")
-                    {
-                        if (args.Length != 1) return 0;
-                        byte[] account = (byte[])args[0];
-                        return BolService.GetBalance(account);
-                    }
-                    if (operation == "decimals") return BolService.Decimals();
-                    if (operation == "register")
-                    {
-                        if (args.Length != 3) return false;
-                        byte[] address = (byte[])args[0];
-                        byte[] codeName = (byte[])args[1];
-                        byte[] edi = (byte[])args[2];
+                    if (args.Length != 1) return 0;
+                    byte[] account = (byte[])args[0];
+                    return BolService.GetBalance(account);
+                }
+                if (operation == "decimals") return BolService.Decimals();
+                if (operation == "register")
+                {
+                    if (args.Length != 3) return false;
+                    byte[] address = (byte[])args[0];
+                    byte[] codeName = (byte[])args[1];
+                    byte[] edi = (byte[])args[2];
 
-                        return BolService.Register(address, codeName, edi);
-                    }
-                    if (operation == "registerCertifier")
-                    {
-                        if (args.Length != 1) return false;
-                        byte[] address = (byte[])args[0];
-                        return BolService.RegisterAsCertifier(address);
-                    }
-                    if (operation == "unregisterCertifier")
-                    {
-                        if (args.Length != 1) return false;
-                        byte[] address = (byte[])args[0];
-                        return BolService.UnregisterAsCertifier(address);
-                    }
-                    if (operation == "certify")
-                    {
-                        if (args.Length != 2) return false;
-                        byte[] certifier = (byte[])args[0];
-                        byte[] address = (byte[])args[1];
-                        return BolService.Certify(certifier, address);
-                    }
-                    if (operation == "uncertify")
-                    {
-                        if (args.Length != 2) return false;
-                        byte[] certifier = (byte[])args[0];
-                        byte[] address = (byte[])args[1];
-                        return BolService.UnCertify(certifier, address);
-                    }
-                    if (operation == "claim")
-                    {
-                        if (args.Length != 1) return false;
-                        byte[] address = (byte[])args[0];
-                        return BolService.Claim(address);
-                    }
+                    return BolService.Register(address, codeName, edi).Serialize();
                 }
-                return false;
+                if (operation == "registerCertifier")
+                {
+                    if (args.Length != 1) return false;
+                    byte[] address = (byte[])args[0];
+                    return BolService.RegisterAsCertifier(address).Serialize();
+                }
+                if (operation == "unregisterCertifier")
+                {
+                    if (args.Length != 1) return false;
+                    byte[] address = (byte[])args[0];
+                    return BolService.UnregisterAsCertifier(address).Serialize();
+                }
+                if (operation == "certify")
+                {
+                    if (args.Length != 2) return false;
+                    byte[] certifier = (byte[])args[0];
+                    byte[] address = (byte[])args[1];
+                    return BolService.Certify(certifier, address).Serialize();
+                }
+                if (operation == "claim")
+                {
+                    if (args.Length != 1) return false;
+                    byte[] address = (byte[])args[0];
+                    return BolService.Claim(address).Serialize();
+                }
             }
-            catch (Exception ex)
-            {
-                Runtime.Log(ex.Message);
-                return false;
-            }
+            return false;
         }
 
         internal static byte[] Sha256Hash(byte[] input)
