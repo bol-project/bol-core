0c4405cd4c53da18b582a514d6a1c65647f928d6
Charalampos Chomenidis <hampos@gmail.com>
2019-06-04 19:55:23 +0300

    Add BolAccount serializable model

---
+++ Bol.Coin/Models/BolAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Bol.Coin.Models
+{
+    [Serializable]
+    public class BolAccount
+    {
+        public byte[] Address;
+        public byte[] CodeName;
+        public byte[] Edi;
+
+        public BigInteger Balance;
+
+        public int Certifications;
+        public byte[][] Certifiers;
+
+        public bool IsCertifier;
+        public BigInteger Collateral;
+
+        public BigInteger RegistrationHeight;
+        public BigInteger LastClaimHeight;
+    }
+}
