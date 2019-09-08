58dae766f00ef64f3bab3101d8f7650df4452236
Charalampos Chomenidis <hampos@gmail.com>
2019-09-08 13:28:27 +0300

    Change proof of work method to compare arithmetic ranges on the first 4 bytes of the script hash instead of base58 characters

--- Bol.Core/Abstractions/IAddressService.cs
+++ Bol.Core/Abstractions/IAddressService.cs
@@ -1,5 +1,6 @@
 ﻿using Bol.Core.Model;
 using Neo.Wallets;
+using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,7 +22,7 @@ namespace Bol.Core.Abstractions
 
         /// <summary>
         /// Generates a BoL B Address with a predefined nonce, bypassing the proof of work.
-        /// A BoL B Address starts with character B and ends with BBB.
+        /// A BoL B Address starts with the prefix BBBB.
         /// The B Address can only be used to claim bonuses. It can only interact with C Addresses of the same CodeName.
         /// </summary>
         /// <param name="codeName"></param>
@@ -32,12 +33,23 @@ namespace Bol.Core.Abstractions
 
         /// <summary>
         /// Generates a BoL C Address.
-        /// A BoL C Address starts with character C and does not have ending restrictions.
+        /// A BoL C Address starts with with the prefix BCCC.
         /// The C Address can be used for any kind of transaction, except claiming bonuses.
         /// </summary>
         /// <param name="codeName"></param>
         /// <param name="publicKey"></param>
         /// <returns></returns>
-        BolAddress GenerateAddressC(string codeName, KeyPair keyPair);
+        BolAddress GenerateAddressC(string codeName, KeyPair keyPair, byte[] nonce);
+
+        /// <summary>
+        /// Calculates a nonce that can satisfy the given arithmetic range by executing a proof of work.
+        /// </summary>
+        /// <param name="codeNameKeyPair"></param>
+        /// <param name="privateKeyPair"></param>
+        /// <param name="rangeFrom"></param>
+        /// <param name="rangeTo"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task<byte[]> CalculateNonceAsync(KeyPair codeNameKeyPair, KeyPair privateKeyPair, uint rangeFrom, uint rangeTo, CancellationToken token = default);
     }
 }
--- Bol.Core/Services/AddressService.cs
+++ Bol.Core/Services/AddressService.cs
@@ -1,10 +1,11 @@
 ﻿using Bol.Core.Abstractions;
-using Bol.Core.Encoders;
 using Bol.Core.Hashers;
 using Bol.Core.Model;
+using Neo;
 using Neo.SmartContract;
 using Neo.Wallets;
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,9 +15,12 @@ namespace Bol.Core.Services
 {
     public class AddressService : IAddressService
     {
-        public const byte B_ADDRESS_PREFIX = 0x99;
-        public const byte C_ADDRESS_PREFIX = 0xAA;
-        public const string B_ADDRESS_START = "BBBB";
+        public const byte B_ADDRESS_PREFIX = 0x19;
+
+        private static uint B_ADDRESS_START = 0x1949C6B9;
+        private static uint B_ADDRESS_END = 0x1949C78C;
+        private static uint C_ADDRESS_START = 0x1954F01A;
+        private static uint C_ADDRESS_END = 0x1954F0ED;
 
         private readonly ISha256Hasher _sha256Hasher;
 
@@ -25,7 +29,37 @@ namespace Bol.Core.Services
             _sha256Hasher = sha256Hasher ?? throw new ArgumentNullException(nameof(sha256Hasher));
         }
 
-        public async Task<BolAddress> GenerateAddressBAsync(string codeName, KeyPair keyPair, CancellationToken token = default)
+        public Task<BolAddress> GenerateAddressBAsync(string codeName, KeyPair keyPair, CancellationToken token = default)
+        {
+            return GenerateAddressAsync(codeName, keyPair, B_ADDRESS_START, B_ADDRESS_END, token);
+        }
+
+        public Task<BolAddress> GenerateAddressCAsync(string codeName, KeyPair keyPair, CancellationToken token = default)
+        {
+            return GenerateAddressAsync(codeName, keyPair, C_ADDRESS_START, C_ADDRESS_END, token);
+        }
+
+        public BolAddress GenerateAddressB(string codeName, KeyPair keyPair, byte[] nonce)
+        {
+            var address = GenerateAddress(codeName, keyPair, nonce);
+            if (!address.Address.StartsWith("BBBB"))
+            {
+                throw new ArgumentException("The provided nonce was invalid and could not create a valid B Address");
+            }
+            return address;
+        }
+
+        public BolAddress GenerateAddressC(string codeName, KeyPair keyPair, byte[] nonce)
+        {
+            var address = GenerateAddress(codeName, keyPair, nonce);
+            if (!address.Address.StartsWith("BCCC"))
+            {
+                throw new ArgumentException("The provided nonce was invalid and could not create a valid C Address");
+            }
+            return address;
+        }
+
+        public async Task<byte[]> CalculateNonceAsync(KeyPair codeNameKeyPair, KeyPair privateKeyPair, uint rangeFrom, uint rangeTo, CancellationToken token = default)
         {
             var parallelChunks = Environment.ProcessorCount;
             var iterations = uint.MaxValue / (uint)parallelChunks;
@@ -40,12 +74,33 @@ namespace Bol.Core.Services
 
                     while (counter <= end)
                     {
-                        var testNonce = BitConverter.GetBytes(counter);
-                        var address = GenerateAddressB(codeName, keyPair, testNonce);
-                        if (address.Address.StartsWith(B_ADDRESS_START))
+                        if (token.IsCancellationRequested)
                         {
-                            return address;
+                            throw new TaskCanceledException();
                         }
+
+                        var testNonce = BitConverter.GetBytes(counter);
+
+                        //Extend the private key with a random nonce
+                        var extendedPrivateKey = _sha256Hasher.Hash(privateKeyPair.PrivateKey.Concat(testNonce).ToArray());
+                        var extendedPrivateKeyPair = new KeyPair(extendedPrivateKey);
+
+                        var scriptHash = CreateScriptHash(codeNameKeyPair, extendedPrivateKeyPair);
+
+                        //Add address prefix byte at start
+                        var addressHash = new[] { B_ADDRESS_PREFIX }.Concat(scriptHash.ToArray());
+
+                        //Take the first 4 bytes of the hash for range comparison
+                        addressHash = addressHash.Take(4).ToArray();
+
+                        var scriptNumber = uint.Parse(addressHash.ToHexString(), NumberStyles.HexNumber);
+
+                        //Scripthash must be in the specified range for successful proof of work
+                        if (rangeFrom <= scriptNumber && scriptNumber <= rangeTo)
+                        {
+                            return testNonce;
+                        }
+
                         counter++;
                     }
 
@@ -57,17 +112,20 @@ namespace Bol.Core.Services
             return await result;
         }
 
-        public BolAddress GenerateAddressB(string codeName, KeyPair keyPair, byte[] nonce)
+        protected async Task<BolAddress> GenerateAddressAsync(string codeName, KeyPair keyPair, uint rangeFrom, uint rangeTo, CancellationToken token = default)
         {
             var hashedCodeName = _sha256Hasher.Hash(Encoding.ASCII.GetBytes(codeName));
             var codeNameKeyPair = new KeyPair(hashedCodeName);
             var codeNameAddress = Contract.CreateSignatureContract(codeNameKeyPair.PublicKey).Address;
 
+            var nonce = await CalculateNonceAsync(codeNameKeyPair, keyPair, rangeFrom, rangeTo, token);
+
             var extendedPrivateKey = _sha256Hasher.Hash(keyPair.PrivateKey.Concat(nonce).ToArray());
             var extendedPrivateKeyPair = new KeyPair(extendedPrivateKey);
             var internalAddress = Contract.CreateSignatureContract(extendedPrivateKeyPair.PublicKey).Address;
 
-            var address = GenerateAddress(codeNameKeyPair, extendedPrivateKeyPair);
+            var address = CreateAddress(codeNameKeyPair, extendedPrivateKeyPair);
+
             return new BolAddress
             {
                 Address = address,
@@ -81,32 +139,42 @@ namespace Bol.Core.Services
             };
         }
 
-        public BolAddress GenerateAddressC(string codeName, KeyPair keyPair)
+        protected BolAddress GenerateAddress(string codeName, KeyPair keyPair, byte[] nonce)
         {
             var hashedCodeName = _sha256Hasher.Hash(Encoding.ASCII.GetBytes(codeName));
             var codeNameKeyPair = new KeyPair(hashedCodeName);
             var codeNameAddress = Contract.CreateSignatureContract(codeNameKeyPair.PublicKey).Address;
 
-            var internalAddress = Contract.CreateSignatureContract(keyPair.PublicKey).Address;
+            var extendedPrivateKey = _sha256Hasher.Hash(keyPair.PrivateKey.Concat(nonce).ToArray());
+            var extendedPrivateKeyPair = new KeyPair(extendedPrivateKey);
+            var internalAddress = Contract.CreateSignatureContract(extendedPrivateKeyPair.PublicKey).Address;
 
-            var address = GenerateAddress(codeNameKeyPair, keyPair);
+            var address = CreateAddress(codeNameKeyPair, extendedPrivateKeyPair);
+
             return new BolAddress
             {
                 Address = address,
-                AddressType = AddressType.C,
-                Nonce = 0,
+                AddressType = AddressType.B,
+                Nonce = BitConverter.ToUInt32(nonce, 0),
                 CodeName = codeName,
                 CodeNameAddress = codeNameAddress,
                 InternalAddress = internalAddress,
                 CodeNamePublicKey = codeNameKeyPair.PublicKey.ToString(),
-                InternalPublicKey = keyPair.PublicKey.ToString()
+                InternalPublicKey = extendedPrivateKeyPair.PublicKey.ToString()
             };
         }
 
-        protected string GenerateAddress(KeyPair codeNameKeyPair, KeyPair secretKeyPair)
+        protected string CreateAddress(KeyPair codeNameKeyPair, KeyPair secretKeyPair)
         {
             var addressContract = Contract.CreateMultiSigContract(2, codeNameKeyPair.PublicKey, secretKeyPair.PublicKey);
             return addressContract.Address;
+        }
+
+        protected UInt160 CreateScriptHash(params KeyPair[] keyPairs)
+        {
+            var publicKeys = keyPairs.Select(kp => kp.PublicKey).ToArray();
+            var addressContract = Contract.CreateMultiSigContract(publicKeys.Length, publicKeys);
+            return addressContract.ScriptHash;
         }
     }
 }
