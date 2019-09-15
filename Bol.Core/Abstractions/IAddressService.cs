7a2c19c90d70e0080d7495cf613830a83c41c0af
Charalampos Chomenidis <hampos@gmail.com>
2019-09-15 12:39:13 +0300

    Correct comments in IAddressService

--- Bol.Core/Abstractions/IAddressService.cs
+++ Bol.Core/Abstractions/IAddressService.cs
@@ -1,4 +1,4 @@
-﻿using Bol.Core.Model;
+using Bol.Core.Model;
 using Neo.Wallets;
 using System.Numerics;
 using System.Threading;
@@ -12,11 +12,11 @@ namespace Bol.Core.Abstractions
     public interface IAddressService
     {
         /// <summary>
-        /// Generates a BoL B Address by doing a proof of work.
-        /// A BoL B Address starts with character B and ends with BBB.
+        /// Generates a BoL B-Address by doing a proof of work.
+        /// A BoL B-Address starts with the character 'B' followed by 3 consecutive 'B' ('BBBB')
         /// The work is completed by finding a Nonce that when combined with the given PublicKey,
-        /// it will generate an Address that ends with BBBB.
-        /// The B Address can only be used to claim bonuses. It can only interact with C Addresses of the same CodeName.
+        /// it will generate an Address that ends with 'BBBB'.
+        /// The B-Address can only be used to claim bonuses. It can only interact with C Addresses of the same CodeName.
         /// </summary>
         /// <param name="codeName"></param>
         /// <param name="publicKey"></param>
@@ -24,9 +24,22 @@ namespace Bol.Core.Abstractions
         Task<BolAddress> GenerateAddressBAsync(string codeName, KeyPair keyPair, CancellationToken token = default);
 
         /// <summary>
-        /// Generates a BoL B Address with a predefined nonce, bypassing the proof of work.
-        /// A BoL B Address starts with the prefix BBBB.
-        /// The B Address can only be used to claim bonuses. It can only interact with C Addresses of the same CodeName.
+        /// Generates a BoL C-Address by doing a proof of work.
+        /// A BoL C-Address starts with the character 'B' followed by 3 consecutive 'C' ('BCCC')
+        /// The work is completed by finding a Nonce that when combined with the given PublicKey,
+        /// it will generate an Address that ends with 'BCCC'.
+        /// The C-Address can be used for any kind of transaction, except claiming bonuses.
+        /// </summary>
+        /// <param name="codeName"></param>
+        /// <param name="keyPair"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task<BolAddress> GenerateAddressCAsync(string codeName, KeyPair keyPair, CancellationToken token = default);
+
+        /// <summary>
+        /// Generates a BoL B-Address with a predefined nonce, bypassing the proof of work.
+        /// A BoL B-Address starts with the character 'B' followed by 3 consecutive 'B' ('BBBB')
+        /// The B-Address can only be used to claim bonuses. It can only interact with C Addresses of the same CodeName.
         /// </summary>
         /// <param name="codeName"></param>
         /// <param name="publicKey"></param>
@@ -35,9 +48,9 @@ namespace Bol.Core.Abstractions
         BolAddress GenerateAddressB(string codeName, KeyPair keyPair, byte[] nonce);
 
         /// <summary>
-        /// Generates a BoL C Address.
-        /// A BoL C Address starts with with the prefix BCCC.
-        /// The C Address can be used for any kind of transaction, except claiming bonuses.
+        /// Generates a BoL C-Address with a predefined nonce, bypassing the proof of work.
+        /// A BoL C-Address starts with the character 'B' followed by 3 consecutive 'C' (BCCC)
+        /// The C-Address can be used for any kind of transaction, except claiming bonuses.
         /// </summary>
         /// <param name="codeName"></param>
         /// <param name="publicKey"></param>
