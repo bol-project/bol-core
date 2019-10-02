47a296d743604ba31a42ada372ccf40e9f44d1b1
Charalampos Chomenidis <hampos@gmail.com>
2019-10-03 00:33:24 +0300

    Add BolResponse (WIP)

---
+++ Bol.Core/Model/BolResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bol.Core.Model
+{
+    public class BolResponse
+    {
+        public bool Success { get; set; }
+        public string TransactionId { get; set; }        
+    }
+}
