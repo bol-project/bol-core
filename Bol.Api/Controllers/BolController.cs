fbd5b4ccf76b0b630deb637ed0ad1a6c4385005d
Charalampos Chomenidis <hampos@gmail.com>
2019-10-03 00:40:46 +0300

    Add BolController (WIP)

---
+++ Bol.Api/Controllers/BolController.cs
@@ -0,0 +1,38 @@
+﻿using Bol.Core.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Bol.Api.Controllers
+{
+    [Route("api/bol")]
+    public class BolController : ControllerBase
+    {
+        private IBolService _bolService;
+
+        public BolController(IBolService bolService)
+        {
+            _bolService = bolService ?? throw new ArgumentNullException(nameof(bolService));
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register()
+        {
+            var result = _bolService.Register();
+            return Ok(result);
+        }
+
+        [HttpPost("claim")]
+        public ActionResult Claim()
+        {
+            var result = _bolService.Claim();
+            return Ok(result);
+        }
+
+        [HttpGet("decimals")]
+        public ActionResult Decimals()
+        {
+            var result = _bolService.Decimals();
+            return Ok(result);
+        }
+    }
+}
