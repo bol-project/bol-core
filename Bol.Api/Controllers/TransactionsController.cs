c2545c493d45d0bd2dbfc38de59a8cd6731418f4
Charalampos Chomenidis <hampos@gmail.com>
2019-10-02 23:30:45 +0300

    Add TransactionsController

---
+++ Bol.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neo;
+using Neo.Ledger;
+using System.Linq;
+
+namespace Bol.Api.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var transactions = Blockchain.Singleton.Store.GetTransactions().Find();
+            var count = transactions.Count();
+            return Ok(transactions);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            return Ok(Blockchain.Singleton.GetTransaction(UInt256.Parse(id)).ToJson());
+        }
+    }
+}
