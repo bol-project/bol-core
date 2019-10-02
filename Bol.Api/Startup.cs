1bcc5ab91f65fa5e741a8183e834a0089a868ae2
Charalampos Chomenidis <hampos@gmail.com>
2019-10-03 00:40:55 +0300

    Add registrations

--- Bol.Api/Startup.cs
+++ Bol.Api/Startup.cs
@@ -1,10 +1,17 @@
-﻿using Bol.Api.BackgroundServices;
+﻿using Akka.Actor;
+using Bol.Api.BackgroundServices;
+using Bol.Core.Abstractions;
+using Bol.Core.Accessors;
+using Bol.Core.Encoders;
+using Bol.Core.Hashers;
+using Bol.Core.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Neo.Shell;
+using Neo.Wallets.NEP6;
 using Prometheus;
 using System;
 
@@ -27,6 +34,16 @@ namespace Bol.Api
             services.AddOpenApiDocument(document => document.DocumentName = "v1");
 
             services.AddSingleton<IHostedService, NodeBackgroundService>();
+
+            services.AddScoped<IContractService, ContractService>();
+            services.AddScoped<IBolService, BolService>();
+            services.AddScoped<IWalletService, WalletService>();
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<ISha256Hasher, Sha256Hasher>();
+            services.AddScoped<IBase16Encoder, Base16Encoder>();
+            services.AddScoped<IBase58Encoder, Base58Encoder>();
+            services.AddScoped<IContextAccessor>((sp) => new WalletContextAccessor(Neo.Program.Wallet as NEP6Wallet));
+            services.AddScoped<IActorRef>((sp) => MainService.System.LocalNode);
 
             return services.BuildServiceProvider();
         }
