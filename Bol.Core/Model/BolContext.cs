f302bf70c86bdf9b4e11ccd1be735e5998c4df94
Charalampos Chomenidis <hampos@gmail.com>
2019-10-03 00:31:27 +0300

    Add BolContext that holds keys and properties of current user

---
+++ Bol.Core/Model/BolContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Neo;
+using Neo.Wallets;
+
+namespace Bol.Core.Model
+{
+    public class BolContext
+    {
+        public string CodeName { get; private set; }
+        public string Edi { get; private set; }
+        public KeyPair CodeNameKey { get; private set; }
+        public KeyPair PrivateKey { get; private set; }
+        public UInt160 BAddress { get; private set; }
+
+        public BolContext(string codeName, string edi, KeyPair codeNameKey, KeyPair privateKey, UInt160 bAddress)
+        {
+            CodeName = codeName ?? throw new ArgumentNullException(nameof(codeName));
+            Edi = edi ?? throw new ArgumentNullException(nameof(edi));
+            CodeNameKey = codeNameKey ?? throw new ArgumentNullException(nameof(codeNameKey));
+            PrivateKey = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
+            BAddress = bAddress ?? throw new ArgumentNullException(nameof(bAddress));
+        }
+    }
+}
