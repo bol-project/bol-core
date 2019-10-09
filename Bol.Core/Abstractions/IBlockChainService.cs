f57078d96bbd886b1bb69f48331848353c2ea3bd
nikosChomenidis <nchomenidis@indeavor.com>
2019-10-09 23:46:27 +0300

    Update IBlockChainInterface with methods `GetCurrentBlock`, GetBlock(id), BetBlocks()`

--- Bol.Core/Abstractions/IBlockChainService.cs
+++ Bol.Core/Abstractions/IBlockChainService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Bol.Core.Dtos;
 using Bol.Core.Model.Internal;
 
 namespace Bol.Core.Abstractions
@@ -5,5 +7,8 @@ namespace Bol.Core.Abstractions
     public interface IBlockChainService
     {
         GetContractResult GetContract(string contract);
+        BlockDto GetCurrentBlock();
+        BlockDto GetBlock(string id);
+        IEnumerable<BaseBlockDto> GetBlocks();
     }
 }
