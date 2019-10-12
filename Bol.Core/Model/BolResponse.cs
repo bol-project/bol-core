115edd6e541bde919738183adc9e2d77609299a1
Charalampos Chomenidis <hampos@gmail.com>
2019-10-12 21:07:20 +0300

    Add Result property on BolResponse

--- Bol.Core/Model/BolResponse.cs
+++ Bol.Core/Model/BolResponse.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Bol.Core.Model
 {
     public class BolResponse
     {
         public bool Success { get; set; }
-        public string TransactionId { get; set; }        
+        public string TransactionId { get; set; }      
+        public string Result { get; set; }
     }
 }
