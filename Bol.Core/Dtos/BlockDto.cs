82c057a49e6a68371dcd11c266a36ae1dafe806b
nikosChomenidis <nchomenidis@indeavor.com>
2019-10-13 02:11:42 +0300

    Update BlockDto to inherit from BaseBlockDto and add properties needed

--- Bol.Core/Dtos/BlockDto.cs
+++ Bol.Core/Dtos/BlockDto.cs
@@ -2,16 +2,19 @@ using System.Collections.Generic;
 
 namespace Bol.Core.Dtos
 {
-    public class BlockDto
+    public class BlockDto : BaseBlockDto
     {
-        public string Hash;
-        public string PrevHash;
-        public string MerkleRoot;
-        public uint Timestamp;
-        public uint Height;
-        public uint Version;
+        public string MerkleRoot; 
+        public string VerificationScript { get; set; }
+        public string InvocationScript { get; set; }
+        public string PreviousBlock { get; set; }
+        public string NextBlock { get; set; } // Find this property
+        public uint Version; // Consider this property (maybe this is not needed)
         public ulong ConsensusData;
         public string NextConsensus;
+        public int NumberOfTransactions { get; set; }
         public IEnumerable<string> Transactions { get; set; }
+
+        // TODO regarding the Bol related data, consider another DTO or even different API calls
     }
 }
