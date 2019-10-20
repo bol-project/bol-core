96d451d3b7e059b94520e12de85d040abffaacfd
Charalampos Chomenidis <hampos@gmail.com>
2019-10-20 23:47:49 +0300

    Add error notifications on bad argument lengths, Add getAccount and addCommercialAddress operations

--- Bol.Coin/Bol.cs
+++ Bol.Coin/Bol.cs
@@ -1,9 +1,8 @@
-using Bol.Coin.Helpers;
-using Bol.Coin.Services;
-using Neo.SmartContract.Framework;
-using Neo.SmartContract.Framework.Services.Neo;
 using System;
 using System.Numerics;
+using Bol.Coin.Models;
+using Bol.Coin.Services;
+using Neo.SmartContract.Framework.Services.Neo;
 
 namespace Neo.SmartContract
 {
@@ -24,53 +23,106 @@ namespace Neo.SmartContract
                 if (operation == "symbol") return BolService.Symbol();
                 if (operation == "transfer")
                 {
-                    if (args.Length != 3) return false;
-                    byte[] from = (byte[])args[0];
-                    byte[] to = (byte[])args[1];
-                    BigInteger value = (BigInteger)args[2];
-                    //Runtime.Log(value.AsByteArray().AsString());
-                    return JsonHelper.AsJson(BolService.Transfer(from, to, value));
+                    if (args.Length != 3)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var from = (byte[])args[0];
+                    var to = (byte[])args[1];
+                    var value = (BigInteger)args[2];
+
+                    return BolService.Transfer(from, to, value);
                 }
                 if (operation == "balanceOf")
                 {
-                    if (args.Length != 1) return 0;
-                    byte[] account = (byte[])args[0];
+                    if (args.Length != 1)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var account = (byte[])args[0];
+
                     return BolService.GetBalance(account);
                 }
                 if (operation == "decimals") return BolService.Decimals();
                 if (operation == "register")
                 {
-                    if (args.Length != 3) return false;
-                    byte[] address = (byte[])args[0];
-                    byte[] codeName = (byte[])args[1];
-                    byte[] edi = (byte[])args[2];
+                    if (args.Length != 3)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var address = (byte[])args[0];
+                    var codeName = (byte[])args[1];
+                    var edi = (byte[])args[2];
 
-                    return JsonHelper.AsJson(BolService.Register(address, codeName, edi));
+                    return BolService.Register(address, codeName, edi);
                 }
                 if (operation == "registerCertifier")
                 {
-                    if (args.Length != 1) return false;
-                    byte[] address = (byte[])args[0];
-                    return JsonHelper.AsJson(BolService.RegisterAsCertifier(address));
+                    if (args.Length != 1)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var address = (byte[])args[0];
+
+                    return BolService.RegisterAsCertifier(address);
                 }
                 if (operation == "unregisterCertifier")
                 {
-                    if (args.Length != 1) return false;
-                    byte[] address = (byte[])args[0];
-                    return JsonHelper.AsJson(BolService.UnregisterAsCertifier(address));
+                    if (args.Length != 1)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var address = (byte[])args[0];
+
+                    return BolService.UnregisterAsCertifier(address);
                 }
                 if (operation == "certify")
                 {
-                    if (args.Length != 2) return false;
-                    byte[] certifier = (byte[])args[0];
-                    byte[] address = (byte[])args[1];
-                    return JsonHelper.AsJson(BolService.Certify(certifier, address));
+                    if (args.Length != 2)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var certifier = (byte[])args[0];
+                    var address = (byte[])args[1];
+
+                    return BolService.Certify(certifier, address);
                 }
                 if (operation == "claim")
                 {
-                    if (args.Length != 1) return false;
-                    byte[] address = (byte[])args[0];
-                    return JsonHelper.AsJson(BolService.Claim(address));
+                    if (args.Length != 1)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var address = (byte[])args[0];
+                    return BolService.Claim(address);
+                }
+                if (operation == "getAccount")
+                {
+                    if (args.Length != 1)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var mainAddress = (byte[])args[0];
+                    return BolService.GetAccount(mainAddress);
+                }
+                if (operation == "addCommercialAddress")
+                {
+                    if (args.Length != 2)
+                    {
+                        Runtime.Notify("error", BolResult.BadRequest("Bad number of arguments"));
+                        return false;
+                    }
+                    var mainAddress = (byte[])args[0];
+                    var commercialAddress = (byte[])args[1];
+                    return BolService.AddCommercialAddress(mainAddress, commercialAddress);
                 }
             }
             return false;
