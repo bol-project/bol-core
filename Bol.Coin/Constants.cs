82bdd637884da38e3dac84d7657d46dc73481b98
John Stylianou <jsm@hotmail.gr>
2020-02-25 13:20:22 +0200

    Claim & constants it uses

--- Bol.Coin/Constants.cs
+++ Bol.Coin/Constants.cs
@@ -20,6 +20,20 @@ namespace Bol.Coin
 
         public static readonly BigInteger DPS = 184200000;
 
+        /*
+       
+        */
+        
+        public static readonly BigInteger[] DpsYear = { 184661436, 187819619, 190637490, 193569717, 196034672, 199614745, 202682166, 205775244, 208348971, 212137640, 215434148, 218821347 };
+        public static readonly BigInteger[] PopYear = { 7713468205, 7794798729, 7874965732, 7953952577, 8031800338, 8108605255, 8184437453, 8259276651, 8333078318, 8405863301, 8477660723, 8548487371 };
+        public static readonly BigInteger[] yearStampSeconds = { 1561939200, 1593561600, 1625097600, 1656633600, 1688169600, 1719792000, 1751328000, 1782864000, 1814400000, 1846022400, 1877558400, 1909094400 };
+        public static readonly BigInteger[] yearStampMiliSeconds = { 1561939200000, 1593561600000, 1625097600000, 1656633600000, 1688169600000, 1719792000000, 1751328000000, 1782864000000, 1814400000000, 1846022400000, 1877558400000, 1909094400000 };
+        public static readonly BigInteger SecOfYear = 31536000;
+        public static readonly BigInteger SecOfLeapYear = 31622400;
+        
+        /*
+         
+        */
         public const byte ACCOUNT_STATUS_OPEN = 0x01;
         public const byte ACCOUNT_STATUS_PENDING_CERTIFICATIONS = 0x02;
         public const byte ACCOUNT_STATUS_PENDING_FEES = 0x03;
