67b5b423ff21ad98fcdd974368f8b977a175aee8
Charalampos Chomenidis <hampos@me.com>
2020-10-11 22:17:48 +0300

    Add IAddressTransformer abstraction

---
+++ Bol.Address/Abstractions/IAddressTransformer.cs
@@ -0,0 +1,8 @@
+﻿namespace Bol.Address
+{
+    public interface IAddressTransformer
+    {
+        string ToAddress(IScriptHash scriptHash);
+        IScriptHash ToScriptHash(string address);
+    }
+}
