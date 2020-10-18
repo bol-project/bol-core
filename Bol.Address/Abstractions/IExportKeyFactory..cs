f1367c47f5adaf64b72957d73056d632bdf251b4
Alex <alexandros_koulouris@hotmail.com>
2020-10-17 23:05:13 -0400

    Add IExportKeyFactory  and IXor

---
+++ Bol.Address/Abstractions/IExportKeyFactory..cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bol.Address.Abstractions
+{
+    public interface IExportKeyFactory
+    {
+        string Export(byte[] PrivateKey, IScriptHash scriptHash,string passphrase, int N, int r, int p);
+    }
+}
