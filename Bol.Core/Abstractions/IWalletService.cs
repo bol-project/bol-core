833012f31d9f5b3c99780c89a3d15d098cd2405b
Charalampos Chomenidis <hampos@me.com>
2020-11-07 18:19:50 +0200

    Change CreateWallet signature to return BolWallet instead of string

--- Bol.Core/Abstractions/IWalletService.cs
+++ Bol.Core/Abstractions/IWalletService.cs
@@ -1,11 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
-using Neo.Wallets.NEP6;
+using Bol.Core.Model.Wallet;
 
 namespace Bol.Core.Abstractions
 {
     public interface IWalletService
     {
-        Task<string> CreateWallet(string walletPassword, string codeName, string edi, string privateKey = null, CancellationToken token = default);
+        Task<BolWallet> CreateWallet(string walletPassword, string codeName, string edi, string privateKey = null, CancellationToken token = default);
     }
 }
