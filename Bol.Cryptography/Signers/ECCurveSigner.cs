f9980beabf68ff9d8ffc4887c5483858b51e7a5b
Charalampos Chomenidis <hampos@me.com>
2021-01-04 21:28:21 +0200

    Add ECCurveSigner to create ECCurve signatures on raw bytes

---
+++ Bol.Cryptography/Signers/ECCurveSigner.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Security.Cryptography;
+using Bol.Cryptography.Abstractions;
+
+namespace Bol.Cryptography.Signers
+{
+    public class ECCurveSigner : IECCurveSigner
+    {
+        public byte[] Sign(byte[] message, byte[] prikey, byte[] pubkey)
+        {
+            using (var ecdsa = ECDsa.Create(new ECParameters
+            {
+                Curve = ECCurve.NamedCurves.nistP256,
+                D = prikey,
+                Q = new ECPoint
+                {
+                    X = pubkey.Take(32).ToArray(),
+                    Y = pubkey.Skip(32).ToArray()
+                }
+            }))
+            {
+                return ecdsa.SignData(message, HashAlgorithmName.SHA256);
+            }
+        }
+
+        public bool VerifySignature(byte[] message, byte[] signature, byte[] pubkey)
+        {
+            using (var ecdsa = ECDsa.Create(new ECParameters
+            {
+                Curve = ECCurve.NamedCurves.nistP256,
+                Q = new ECPoint
+                {
+                    X = pubkey.Take(32).ToArray(),
+                    Y = pubkey.Skip(32).ToArray()
+                }
+            }))
+            {
+                return ecdsa.VerifyData(message, signature, HashAlgorithmName.SHA256);
+            }
+        }
+    }
+}
