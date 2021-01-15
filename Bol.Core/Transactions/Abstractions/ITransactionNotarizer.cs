469b7937a5b7357b143351e811e1f89a744a0a68
Charalampos Chomenidis <hampos@me.com>
2021-01-15 04:33:39 +0200

    Add TransactionNotarizer to sign and create Witnesses for a transaction

---
+++ Bol.Core/Transactions/Abstractions/ITransactionNotarizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Bol.Address;
+using Bol.Cryptography;
+
+namespace Bol.Core.Transactions
+{
+    public interface ITransactionNotarizer
+    {
+        BolTransaction Notarize(BolTransaction transaction, ISignatureScript witness, IEnumerable<IKeyPair> keys);
+    }
+}
---
+++ Bol.Core/Transactions/TransactionNotarizer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Bol.Address;
+using Bol.Cryptography;
+//using Neo.VM;
+
+namespace Bol.Core.Transactions
+{
+    public class TransactionNotarizer : ITransactionNotarizer
+    {
+        private readonly ITransactionSigner _signer;
+
+        public BolTransaction Notarize(BolTransaction transaction, ISignatureScript witness, IEnumerable<IKeyPair> keys)
+        {
+            var signatures = keys.OrderBy(key => key.PublicKey)
+                .Select(key => _signer.GenerateSignature(transaction, key))
+                .Select(signature => new byte[] { 0x40 }.Concat(signature).ToArray())
+                .Aggregate( (s1,s2) => s1.Concat(s2).ToArray());
+
+            //using var sb = new ScriptBuilder();
+            //foreach (var signature in signatures)
+            //{
+            //    sb.EmitPush(signature);
+            //}
+
+            transaction.Witnesses = new[]
+            {
+                new BolTransactionWitness
+                {
+                    InvocationScript = signatures,
+                    VerificationScript = witness.GetBytes()
+                }
+            };
+            return transaction;
+        }
+    }
+}
