9612f0d5b013e5f65be045956c586355ec03b0f4
Charalampos Chomenidis <hampos@me.com>
2021-01-16 16:37:29 +0200

    Remove obsolete BolController methods

--- Bol.Api/Controllers/BolController.cs
+++ Bol.Api/Controllers/BolController.cs
@@ -1,16 +1,16 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Bol.Address.Abstractions;
 using Bol.Api.Dtos;
 using Bol.Core.Abstractions;
 using Bol.Core.Model.Wallet;
 using Bol.Cryptography;
 using Microsoft.AspNetCore.Mvc;
-using Neo.IO.Json;
 using Neo.Wallets;
-using System;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+using IBolService = Bol.Api.Services.IBolService;
 
 namespace Bol.Api.Controllers
 {
@@ -48,7 +48,7 @@ namespace Bol.Api.Controllers
 
         });
 
-            var result = _bolService.Create(keys);
+            var result = _bolService.Create(keys.Select(key => new KeyPair(key.PrivateKey)));
 
             return Ok(result);
         }
@@ -65,7 +65,7 @@ namespace Bol.Api.Controllers
                 return _keyPairFactory.Create(_exportKeyFactory.GetDecryptedPrivateKey(accounts.First().Key, "bol", bolWallet.Scrypt.N, bolWallet.Scrypt.R, bolWallet.Scrypt.P));
             });
 
-            var result = _bolService.Deploy(keys);
+            var result = _bolService.Deploy(keys.Select(key => new KeyPair(key.PrivateKey)));
 
             return Ok(result);
         }
@@ -75,41 +75,6 @@ namespace Bol.Api.Controllers
         {
             var result = await _walletService.CreateWallet(request.WalletPassword, request.CodeName, request.Edi, request.PrivateKey, token);
 
-            return Ok(result);
-        }
-
-        [HttpPost("register")]
-        public ActionResult Register()
-        {
-            var result = _bolService.Register();
-            return Ok(result);
-        }
-
-        [HttpPost("claim")]
-        public ActionResult Claim()
-        {
-            var result = _bolService.Claim();
-            return Ok(result);
-        }
-
-        [HttpPost("addCommercialAddress")]
-        public ActionResult Claim(string address)
-        {
-            var result = _bolService.AddCommercialAddress(address.ToScriptHash());
-            return Ok(result);
-        }
-
-        [HttpPost("certify")]
-        public ActionResult Certify(string address)
-        {
-            var result = _bolService.Certify(address.ToScriptHash());
-            return Ok(result);
-        }
-
-        [HttpPost("unCertify")]
-        public ActionResult UnCertify(string address)
-        {
-            var result = _bolService.UnCertify(address.ToScriptHash());
             return Ok(result);
         }
 
@@ -145,13 +110,6 @@ namespace Bol.Api.Controllers
         public ActionResult TotalSupply()
         {
             var result = _bolService.TotalSupply();
-            return Ok(result);
-        }
-
-        [HttpGet("getCertifiers")]
-        public ActionResult TotalSupply(string countryCode)
-        {
-            var result = _bolService.GetCertifiers(countryCode);
             return Ok(result);
         }
     }
