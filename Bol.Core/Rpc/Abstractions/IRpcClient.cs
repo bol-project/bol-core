487c4790d56aa464eae2592cbeac6c01ee0b9b34
Alex <alexandros_koulouris@hotmail.com>
2021-01-28 19:23:43 -0500

    Convert to async method / Returning generic constraint / Handle the Rpc exception

--- Bol.Core/Rpc/Abstractions/IRpcClient.cs
+++ Bol.Core/Rpc/Abstractions/IRpcClient.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Bol.Core.Rpc.Model;
 
 namespace Bol.Core.Rpc.Abstractions
 {
     public interface IRpcClient
     {
-         string InvokeAsync(string hexTx, string method);
+        Task<T> InvokeAsync<T>(string hexTx, string method, CancellationToken token = default);
     }
 }
--- Bol.Core/Rpc/RpcClient.cs
+++ Bol.Core/Rpc/RpcClient.cs
@@ -1,29 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Bol.Core.Abstractions;
 using Bol.Core.Rpc.Abstractions;
 using Bol.Core.Rpc.Model;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace Bol.Core.Rpc
 {
     public class RpcClient : IRpcClient
     {
-        private readonly RpcInfo _RpcInfo;
-        private IJsonSerializer _IJsonSerializer;
+        private readonly RpcInfo _rpcInfo;
+        private IJsonSerializer _iJsonSerializer;
         private static HttpClient _httpClient = new HttpClient();
 
-        public RpcClient(IOptions<RpcInfo> RpcInfo, IJsonSerializer IJsonSerializer)
+        public RpcClient(IOptions<RpcInfo> rpcInfo, IJsonSerializer iJsonSerializer)
         {
 
-            _RpcInfo = RpcInfo.Value ?? throw new ArgumentNullException(nameof(RpcInfo));
-            _IJsonSerializer = IJsonSerializer ?? throw new ArgumentNullException(nameof(IJsonSerializer));
+            _rpcInfo = rpcInfo.Value ?? throw new ArgumentNullException(nameof(rpcInfo));
+            _iJsonSerializer = iJsonSerializer ?? throw new ArgumentNullException(nameof(iJsonSerializer));
         }
-        public string InvokeAsync(string hexTx, string method)
+        public async Task<T> InvokeAsync<T>(string hexTx, string method, CancellationToken token = default) 
         {
-              var url = new Uri("http://" + _RpcInfo.BindAddress + ":"+ _RpcInfo.Port+ "/ ");
+            var url = new Uri("http://" + _rpcInfo.BindAddress + ":" + _rpcInfo.Port + "/ ");
             var request = new RpcRequest
             {
                 id = 1,
@@ -31,12 +35,36 @@ namespace Bol.Core.Rpc
                 method = method,
                 @params = new object[] { hexTx }
             };
-            using (var content = new StringContent(_IJsonSerializer.Serialize(request), System.Text.Encoding.UTF8, "application/json-rpc"))
-            {
-                var result = _httpClient.PostAsync(url, content).Result; 
 
-                return  result.Content.ReadAsStringAsync().Result;
-            }
+
+            var content = new StringContent(_iJsonSerializer.Serialize(request), System.Text.Encoding.UTF8, "application/json-rpc");
+
+                using (var response = await _httpClient.PostAsync(url, content, token))
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (response.Content == null) { throw new ArgumentNullException(nameof(response.Content)); }
+       
+                        var responseJson = await response.Content.ReadAsStringAsync();
+
+                       var jObjectResponse = JObject.Parse(responseJson);
+
+                        if (jObjectResponse.ContainsKey("error"))
+                        {
+                     
+                            throw new RpcException(Int32.Parse(jObjectResponse["error"]["code"].ToString()), jObjectResponse["error"]["message"].ToString());
+                        }
+
+                        if (responseJson == null) { throw new ArgumentNullException(nameof(responseJson)); }
+
+                        return _iJsonSerializer.Deserialize<T>(responseJson);
+                    }
+                    else
+                    {
+                        throw new Exception(response.StatusCode.ToString());
+                    }
+
+                }
 
         }
     }
