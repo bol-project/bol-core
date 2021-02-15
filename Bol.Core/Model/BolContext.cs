6344ff8622278ee2b699fd706cddb4ba995cb655
Charalampos Chomenidis <hampos@me.com>
2021-02-15 09:39:10 +0200

    Initialize Contract property in BolContext constructor

--- Bol.Core/Model/BolContext.cs
+++ Bol.Core/Model/BolContext.cs
@@ -8,6 +8,7 @@ namespace Bol.Core.Model
 {
     public class BolContext
     {
+        public string Contract { get; private set; }
         public string CodeName { get; private set; }
         public string Edi { get; private set; }
         public IKeyPair CodeNameKey { get; private set; }
@@ -16,18 +17,27 @@ namespace Bol.Core.Model
         public KeyValuePair<IScriptHash, IKeyPair> BlockChainAddress { get; private set; }
         public KeyValuePair<IScriptHash, IKeyPair> SocialAddress { get; private set; }
         public IEnumerable<KeyValuePair<IScriptHash, IKeyPair>> CommercialAddresses { get; private set; }
-        public string Contract { get; private set; }
 
-        public BolContext(string codeNameNew, string ediNew, IKeyPair codeNameKeyNew, IKeyPair privateKeyNew, IScriptHash mainAddressNew, KeyValuePair<IScriptHash, IKeyPair> blockChainAddressNew, KeyValuePair<IScriptHash, IKeyPair> socialAddressNew, IEnumerable<KeyValuePair<IScriptHash, IKeyPair>> commercialAddressesNew)
+        public BolContext(
+            string contract,
+            string codeName, 
+            string edi, 
+            IKeyPair codeNameKey, 
+            IKeyPair privateKey, 
+            IScriptHash mainAddress, 
+            KeyValuePair<IScriptHash, IKeyPair> blockChainAddress, 
+            KeyValuePair<IScriptHash, IKeyPair> socialAddress, 
+            IEnumerable<KeyValuePair<IScriptHash, IKeyPair>> commercialAddresses)
         {
-            CodeName = codeNameNew ?? throw new ArgumentNullException(nameof(codeNameNew));
-            Edi = ediNew ?? throw new ArgumentNullException(nameof(ediNew));
-            CodeNameKey = codeNameKeyNew ?? throw new ArgumentNullException(nameof(codeNameKeyNew));
-            PrivateKey = privateKeyNew ?? throw new ArgumentNullException(nameof(privateKeyNew));
-            MainAddress = mainAddressNew ?? throw new ArgumentNullException(nameof(mainAddressNew));
-            BlockChainAddress = blockChainAddressNew;
-            SocialAddress = socialAddressNew;
-            CommercialAddresses = commercialAddressesNew ?? throw new ArgumentNullException(nameof(commercialAddressesNew));
+            Contract = contract ?? throw new ArgumentNullException(nameof(contract));
+            CodeName = codeName ?? throw new ArgumentNullException(nameof(codeName));
+            Edi = edi ?? throw new ArgumentNullException(nameof(edi));
+            CodeNameKey = codeNameKey ?? throw new ArgumentNullException(nameof(codeNameKey));
+            PrivateKey = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
+            MainAddress = mainAddress ?? throw new ArgumentNullException(nameof(mainAddress));
+            BlockChainAddress = blockChainAddress;
+            SocialAddress = socialAddress;
+            CommercialAddresses = commercialAddresses ?? throw new ArgumentNullException(nameof(commercialAddresses));
         }
     
     }   
