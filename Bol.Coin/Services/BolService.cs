df75837177986e09b93a99ea6c5964af272dd6fc
Stathas Petros <stathasp@gmail.com>
2021-03-16 19:25:11 +0000

    Update BolService.cs

--- Bol.Coin/Services/BolService.cs
+++ Bol.Coin/Services/BolService.cs
@@ -807,7 +807,7 @@ namespace Bol.Coin.Services
 
                     intervalDistribute = intervalTime * DpsNC / intervalTotal;
                     BolRepository.SetDistributeAtBlock(i, intervalDistribute);
-                    BolRepository.SetRegisteredAtBlock(i,intervalTotal );
+                    BolRepository.SetRegisteredAtBlock(i, intervalTotal);
                     var intervalBirths = intervalTime * Bps;
                     BolRepository.SetNewBolAtBlock(i, intervalBirths);
                     BolRepository.SetPopulationAtBlock(i, Pop);
