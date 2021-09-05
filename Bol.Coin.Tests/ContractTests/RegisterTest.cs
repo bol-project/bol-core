9ae543b00b28a9379f6a2e74f8265034ab525891
Charalampos Chomenidis <hampos@me.com>
2021-09-05 21:56:48 +0300

    Rework register tests to use Bol Transaction module execution scripts

--- Bol.Coin.Tests/ContractTests/RegisterTest.cs
+++ Bol.Coin.Tests/ContractTests/RegisterTest.cs
@@ -1,110 +1,130 @@
 using System.IO;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Bol.Address;
 using Bol.Address.Model.Configuration;
+using Bol.Address.Neo;
+using Bol.Core.Accessors;
+using Bol.Core.Model;
+using Bol.Core.Model.Wallet;
+using Bol.Core.Rpc.Abstractions;
+using Bol.Core.Services;
+using Bol.Core.Transactions;
 using Bol.Cryptography.Encoders;
 using Bol.Cryptography.Hashers;
-using LunarLabs.Parser;
+using Bol.Cryptography.Keys;
+using Bol.Cryptography.Signers;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 using Neo.Emulation;
 using Neo.Emulation.API;
-using NUnit.Framework;
-using Microsoft.Extensions.Options;
-using System.Collections.Generic;
-using System.Linq;
 using Neo.VM;
-using Neo.IO.Json;
+using Newtonsoft.Json;
+using Xunit;
 
 namespace Bol.Coin.Tests.ContractTests
 {
-    
-    public class RegisterTest
+    public class TransactionGrabber : IRpcMethodFactory
     {
-        private static Emulator emulator;
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            var path = TestContext.CurrentContext.TestDirectory.Replace("Bol.Coin.Tests", "Bol.Coin").Replace("\\net471", "");
-            Directory.SetCurrentDirectory(path);
-            var avmBytes = File.ReadAllBytes("Bol.Coin.avm");
-           var chain = new Blockchain();     
-            emulator = new Emulator(chain);
-       
-            var address = chain.DeployContract("test", avmBytes);
-            emulator.SetExecutingAccount(address);
+        public BolTransaction Transaction { get; set; }
 
-      
+        public Task<T> GetAccount<T>(string mainAddress, CancellationToken token = default)
+        {
+            return Task.FromResult(default(T));
         }
 
-       
-        [Test] 
-        public void TestRegister()
+        public Task<T> SendRawTransaction<T>(BolTransaction transaction, CancellationToken token = default)
         {
-            var hasher = new Sha256Hasher();
-            var base58Encoder = new Base58Encoder(hasher);
-            var base16Encoder = new Base16Encoder(hasher);
-            var protocolConfiguration = new ProtocolConfiguration { AddressVersion = "25" } ;
-            var addressTransformer = new AddressTransformer(base58Encoder, base16Encoder, Options.Create(protocolConfiguration));
-            var mainAddress = addressTransformer.ToScriptHash("BBBBkGYdgXAjThre8FgpQQF7uyx1CwqZ91").ToHexString();
+            Transaction = transaction;
+            return Task.FromResult(default(T));
+        }
 
+        public Task<T> TestRawTransaction<T>(BolTransaction transaction, CancellationToken token = default)
+        {
+            Transaction = transaction;
+            return Task.FromResult(default(T));
+        }
+    }
 
-            var CodeName = base16Encoder.Encode(Encoding.ASCII.GetBytes("P<GRC< PAPPAS < S < MANU < CHAO < 1983MP < LsDDs8n8snS5BCA"));
+    public class RegisterTest
+    {
+        private Emulator _emulator;
+        private BolService _service;
+        private TransactionGrabber _transactionGrabber;
 
-            var edi = "e3274f6bbd018f920e7d629ba035d211e68e41f23f28f5a9f9b89b7b0ea860db";///.HexToBytes();
+        public RegisterTest()
+        {
+            var avmBytes = File.ReadAllBytes("Bol.Coin.avm");
+            var chain = new Blockchain();
+            _emulator = new Emulator(chain);
 
-            var blockchainAddress = addressTransformer.ToScriptHash("BCQgrM8nHNsjGhEMNtCbAMLmWSPQ1BmjTe").ToHexString();
+            var address = chain.DeployContract("BoL", avmBytes);
+            _emulator.SetExecutingAccount(address);
 
-            var socialAddress = addressTransformer.ToScriptHash("BGfALdfQpoByaL5bTEsPetiUhBwm2S7jCh").ToHexString();
+            var walletJson = File.ReadAllText("wallet.json");
 
-            var commercialAddress = new List<byte[]> {
-                addressTransformer.ToScriptHash("B5ZuFhYb9vxZfbE6KeeDW4TMFtMPJrBEgZ").GetBytes(),
-                addressTransformer.ToScriptHash("BDfMZmAYSNJZGenaKzXxqH4DYDU8mqmFWM").GetBytes(),
-                addressTransformer.ToScriptHash("BE3mXKfnMMHqXiuhaQKVVnH6GkYJVYdGTd").GetBytes(),
-                addressTransformer.ToScriptHash("BAnLHX2pCCjnPTSJPZxWvs2tPjLziSAsSx").GetBytes(),
-                addressTransformer.ToScriptHash("BAnyonwLzx4UfrBAYywDJnQ4xYFsTY8GG1").GetBytes(),
-                addressTransformer.ToScriptHash("B7qyoLnVSbVsE7uQhkGMPPQWdV6HhbkrvU").GetBytes(),
-                addressTransformer.ToScriptHash("BQTeWJoNzyrnAjiDcsDCcyvYT814q8U8Tm").GetBytes(),
-                addressTransformer.ToScriptHash("B6QWN2J4xUASrM6tGudBFxoYtR6rukLgb9").GetBytes(),
-                addressTransformer.ToScriptHash("BSWzdj1hqPktS6q1dXfknFuR8eyWKVptHs").GetBytes(),
-                addressTransformer.ToScriptHash("BSizM5YtXQVgzdta6NYkhaUvMKRBmkzyFK").GetBytes(),          
+            var wallet = Options.Create(JsonConvert.DeserializeObject<BolWallet>(walletJson));
+            var walletConfig = Options.Create(new WalletConfiguration { Password = "bol" });
+            var bolConfig = Options.Create(new BolConfig { Contract = "49071c33087967cc6d3c0f0ef35c013163b047eb" });
+            var protocolConfig = Options.Create(new ProtocolConfiguration { AddressVersion = "25" });
 
-        }.SelectMany(a => a).ToArray();
+            var sha256 = new Sha256Hasher();
+            var xor = new Xor();
+            var base58 = new Base58Encoder(sha256);
+            var base16 = new Base16Encoder(sha256);
+            var ripemd160 = new RipeMD160Hasher();
 
-            var operation = DataNode.CreateArray();
-            operation.Name = "register";
-            operation.AddValue("register");
+            var addressTransformer = new AddressTransformer(base58, base16, protocolConfig);
 
-            var args = DataNode.CreateArray();
-            operation.Name = "args";
-            args.AddValue("0x" + mainAddress);
-            args.AddValue("0x" + CodeName);
-            args.AddValue("0x" + edi);
-            args.AddValue("0x" + blockchainAddress);
-            args.AddValue("0x" + socialAddress);
-            args.AddValue("0x" + base16Encoder.Encode(commercialAddress));
-           
-            operation.AddNode(args);
+            var exportKeyFactory = new ExportKeyFactory(sha256, addressTransformer, xor);
+            var keyPairFactory = new KeyPairFactory();
+            var cachingService = new CachingService(new MemoryCache(Options.Create(new MemoryCacheOptions())));
+            var signatureScriptFactory = new SignatureScriptFactory(base16, sha256, ripemd160);
+            var scriptHashFactory = new ScriptHashFactory(base16);
+            var transactionSerializer = new TransactionSerializer();
+            var transactionSigner = new TransactionSigner(transactionSerializer, new ECCurveSigner());
+            var transactionNotarizer = new TransactionNotarizer(transactionSigner);
 
-             var notifyOutput = "";
+            var contextAccessor = new WalletContextAccessor(wallet, walletConfig, bolConfig, exportKeyFactory, keyPairFactory, addressTransformer, cachingService);
+
+            _transactionGrabber = new TransactionGrabber();
+            var transactionService = new TransactionService(signatureScriptFactory, scriptHashFactory, transactionNotarizer, _transactionGrabber);
+
+            _service = new BolService(contextAccessor, transactionService, signatureScriptFactory, base16);
+        }
+
+        [Fact]
+        public async Task TestRegister()
+        {
+            var notifyOutput = "";
             Runtime.OnLogMessage = (string x) =>
             {
                 notifyOutput = x;
             };
-          
-            var loaderScript = emulator.GenerateLoaderScriptFromInputs(operation, null);
-            
-            emulator.checkWitnessMode = CheckWitnessMode.AlwaysTrue;
-            emulator.Reset(loaderScript, null, "register");
-            emulator.Run();
-          
-            var result = emulator.GetOutput();
+            await _service.Deploy();
+            var loaderScript = _transactionGrabber.Transaction.ExecutionScript.GetBytes();
+
+            _emulator.checkWitnessMode = CheckWitnessMode.AlwaysTrue;
+            _emulator.Reset(loaderScript, null, "deploy", table: new ScriptTable());
+            _emulator.Run();
+
+            await _service.Register();
+
+            loaderScript = _transactionGrabber.Transaction.ExecutionScript.GetBytes();
+
+            _emulator.checkWitnessMode = CheckWitnessMode.AlwaysTrue;
+            _emulator.Reset(loaderScript, null, "register", table: new ScriptTable());
+            _emulator.Run();
+
+            var result = _emulator.GetOutput();
             Assert.NotNull(result);
-           
+
             var register = result.GetBoolean();
-             Assert.IsTrue(register.Equals(true));
+            Assert.True(register);
 
-            var notifyOutputObject = JObject.Parse(notifyOutput.ToString());
-            Assert.IsTrue(notifyOutputObject.AsString().Equals("register"));
-
+            var notifyOutputObject = notifyOutput.ToString();
         }
     }
+
+
 }
