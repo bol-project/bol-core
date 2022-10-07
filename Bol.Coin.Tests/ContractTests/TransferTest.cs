709fe56d003a54c784fe84909a2e1c76f00ac17c
Charalampos Chomenidis <hampos@me.com>
2022-10-07 19:10:30 +0300

    Refactor tests to utilise base scenarios for registration and certifications

--- Bol.Coin.Tests/ContractTests/TransferTest.cs
+++ Bol.Coin.Tests/ContractTests/TransferTest.cs
@@ -13,48 +13,13 @@ using Xunit;
 
 namespace Bol.Coin.Tests.ContractTests;
 
-public class TransferTest
+public class TransferTest : TestBase
 {
-    private readonly Emulator _emulator;
-    private readonly BolService _service;
-    private readonly TransactionGrabber _transactionGrabber;
-    private readonly AddressTransformer _addressTransformer;
-    private readonly BolService _validatorService;
-    private string _notifyOutput;
-
-    public TransferTest()
-    {
-        _emulator = EmulatorUtils.Create((string notifyOutput) =>
-        {
-            _notifyOutput = notifyOutput;
-            Console.WriteLine(notifyOutput);
-        });
-        _transactionGrabber = new TransactionGrabber();
-        _service = BolServiceFactory.Create(_transactionGrabber);
-            
-        var protocolConfig = Options.Create(new ProtocolConfiguration { AddressVersion = "25" });
-        var sha256 = new Sha256Hasher();
-        _addressTransformer = new AddressTransformer(new Base58Encoder(sha256), new Base16Encoder(sha256), protocolConfig);
-            
-        _service = BolServiceFactory.Create(_transactionGrabber);
-
-        var blockchainValidatorContext = BolContextFactory.Create("P<GRC<CHOMENIDIS<C<<<1985MP<LsDDs8n8snS5BCA", "BBB9yo34hw2RarigYR3LrcXzrxEPMjojt5");
-        _validatorService = BolServiceFactory.Create(_transactionGrabber, blockchainValidatorContext);
-    }
-
     [Fact]
     public async Task TransferClaim_ShouldExecute()
     {
-        await _service.Deploy();
-        _emulator.Execute(_transactionGrabber);
-
-        await _validatorService.Whitelist(_addressTransformer.ToScriptHash("BBBBkGYdgXAjThre8FgpQQF7uyx1CwqZ91"));
-        _emulator.Execute(_transactionGrabber);
-
-        _emulator.blockchain.AddMockBlocks(100);
-
-        await _service.Register();
-        _emulator.Execute(_transactionGrabber);
+        await WhitelistAndRegister();
+        await AddCertifications();
 
         _emulator.blockchain.AddMockBlocks(100);
 
@@ -71,16 +36,8 @@ public class TransferTest
     [Fact]
     public async Task Transfer_ShouldExecute()
     {
-        await _service.Deploy();
-        _emulator.Execute(_transactionGrabber);
-
-        await _validatorService.Whitelist(_addressTransformer.ToScriptHash("BBBBkGYdgXAjThre8FgpQQF7uyx1CwqZ91"));
-        _emulator.Execute(_transactionGrabber);
-
-        _emulator.blockchain.AddMockBlocks(100);
-
-        await _service.Register();
-        _emulator.Execute(_transactionGrabber);
+        await WhitelistAndRegister();
+        await AddCertifications();
 
         _emulator.blockchain.AddMockBlocks(100);
 
