6c85843208254374bddfa15cd75ee1611db68d72
Charalampos Chomenidis <hampos@me.com>
2022-10-14 00:38:56 +0300

    Refactor wallet creation test to also perform proof of work

--- Bol.Core.Tests/Services/WalletServiceTest.cs
+++ Bol.Core.Tests/Services/WalletServiceTest.cs
@@ -1,58 +1,68 @@
+using System.Linq;
 using Xunit;
 using Bol.Core.Services;
 using Bol.Core.Abstractions;
-using Moq;
-using Bol.Cryptography;
 using Bol.Cryptography.Encoders;
 using Bol.Cryptography.Hashers;
 using Bol.Address.Neo;
-using Bol.Cryptography.Keys;
 using Bol.Address;
 using Microsoft.Extensions.Options;
 using Bol.Address.Model.Configuration;
-using Bol.Core.Model;
 using System.Threading.Tasks;
+using Bol.Core.Helpers;
 using Bol.Cryptography.Neo.Encoders;
 using Bol.Cryptography.Neo.Hashers;
 using Bol.Cryptography.Neo.Keys;
 
 namespace Bol.Core.Tests.Services
 {
-
     public class WalletServiceTest
     {
-        private readonly Mock<IAddressService> _IAddressService;
+        private readonly IWalletService _walletService;
+
         public WalletServiceTest()
         {
-            _IAddressService = new Mock<IAddressService>();
+            var hex = new Base16Encoder(new Sha256Hasher());
+            var sha256 = new Sha256Hasher();
+            var ripemd160 = new RipeMD160Hasher();
+            var base58 = new Base58Encoder(new Sha256Hasher());
+
+            var signatureScriptFactory = new SignatureScriptFactory(hex, sha256, ripemd160);
+            var keyPairFactory = new KeyPairFactory();
+
+            var protocolConfiguration = new ProtocolConfiguration() { AddressVersion = "25" };
+            var addressTransformer = new AddressTransformer(base58, hex, Options.Create(protocolConfiguration));
+            var xor = new Xor();
+            var exportKeyFactory = new ExportKeyFactory(sha256, addressTransformer, xor);
+
+            var nonceCalculator = new NonceCalculator();
+            var addressService = new AddressService(
+                keyPairFactory,
+                signatureScriptFactory,
+                addressTransformer,
+                sha256,
+                hex,
+                nonceCalculator);
+
+            _walletService = new WalletService(
+                addressService,
+                signatureScriptFactory,
+                keyPairFactory,
+                sha256,
+                addressTransformer,
+                exportKeyFactory,
+                hex);
         }
+
         [Fact]
-        public async System.Threading.Tasks.Task Generate_WalletAsync()
+        public async Task Generate_WalletAsync()
         {
-            // var taskCompletion = new TaskCompletionSource<BolAddress>();
-            // taskCompletion.SetResult(v);
-
-            //Mock IAddressService
-            var KeyPair = new Mock<IKeyPair>();
-            var bolAddress = new BolAddress() { Nonce = 2505398938 };                    
-            _IAddressService.Setup(m => m.GenerateAddressBAsync(It.IsAny<string>(), It.IsAny<KeyPair>(), default)).Returns(Task.FromResult<BolAddress>(bolAddress));
-
-            var Base16Encoder = new Base16Encoder(new Sha256Hasher());
-            var Sha256Hasher = new Sha256Hasher();
-            var RipeMD160Hasher = new RipeMD160Hasher();
-            var Base58Encoder = new Base58Encoder(new Sha256Hasher());
-
-            var signatureScriptFactory = new SignatureScriptFactory(Base16Encoder, Sha256Hasher, RipeMD160Hasher);
-            var KeyPairFactory = new KeyPairFactory();
-
+            var bolWallet = await _walletService.CreateWallet("123456",
+                "P<GRC<PAPPAS<S<MANU<CHAO<1983MP<LsDDs8n8snS5BCA",
+                "4F743113AF44B152C3D2D818DE91C808D104E14F612C268D7CA9D0A477F48D0C",
+                "CD8B78CD37AC684D82E066C95ED6995446B9FFB3E3F5028092FF0248C0C37B79");
             
-            var Protocolconf = new  ProtocolConfiguration() { AddressVersion = "25" };
-            var AddressTransformer = new AddressTransformer(Base58Encoder, Base16Encoder, Options.Create(Protocolconf));
-            var Xor = new Xor();
-            var ExportKeyFactory = new ExportKeyFactory(Sha256Hasher, AddressTransformer, Xor);
-
-            var WalletService = new WalletService(_IAddressService.Object, signatureScriptFactory, KeyPairFactory, Sha256Hasher, AddressTransformer, ExportKeyFactory, Base16Encoder);
-            var bolWallet =   await WalletService.CreateWallet("123456", "P<GRC<PAPPAS<S<MANU<CHAO<1983MP<LsDDs8n8snS5BCA", "4F743113AF44B152C3D2D818DE91C808D104E14F612C268D7CA9D0A477F48D0C", "CD8B78CD37AC684D82E066C95ED6995446B9FFB3E3F5028092FF0248C0C37B79");
+            Assert.StartsWith("BBB", bolWallet.accounts.First().Address);
         }
     }
 }
