ec5a953580276f3f82e23667d2f367ca7e5a84e6
Charalampos Chomenidis <hampos@me.com>
2023-12-26 13:04:17 +0200

    Add NumberEllipsis method that splits a string at position Min(length/2, maxChars) and replaces the rest with the number of remaining characters

--- Bol.Core/Serializers/PersonStringSerializer.cs
+++ Bol.Core/Serializers/PersonStringSerializer.cs
@@ -119,5 +119,14 @@ namespace Bol.Core.Serializers
 
             return gender;
         }
+
+        internal string NumberEllipsis(string input, int maxChars)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return input;
+            
+            var length = input.Length;
+            var take = Math.Min(input.Length / 2, maxChars);
+            return $"{input.Substring(0, take)}{length-take}";
+        }
     }
 }
