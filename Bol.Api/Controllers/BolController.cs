16fe130f06cd08bbf28e7cadaf95693f2dfe7202
Charalampos Chomenidis <hampos@me.com>
2024-01-11 22:35:50 +0200

    Add register and unregister as certifier methods and cleanup unused methods

--- Bol.Api/Controllers/BolController.cs
+++ Bol.Api/Controllers/BolController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
@@ -9,7 +10,6 @@ using Bol.Api.Dtos;
 using Bol.Core.Abstractions;
 using Bol.Cryptography;
 using Microsoft.AspNetCore.Mvc;
-using IBolService = Bol.Api.Services.IBolService;
 using Bol.Address;
 using Bol.Core.Model;
 using Neo;
@@ -19,44 +19,46 @@ namespace Bol.Api.Controllers
     [Route("api/bol")]
     public class BolController : ControllerBase
     {
-        private readonly IBolService _bolService;
         private readonly IWalletService _walletService;
         private readonly IExportKeyFactory _exportKeyFactory;
         private readonly IJsonSerializer _jsonSerializer;
         private readonly IKeyPairFactory _keyPairFactory;
 
-        private readonly Bol.Core.Abstractions.IBolService _coreBolService;
+        private readonly IBolService _bolService;
         private readonly IAddressTransformer _addressTransformer;
 
-        public BolController(IBolService bolService,
+        public BolController(
             IWalletService walletService,
             IExportKeyFactory exportKeyFactory,
             IJsonSerializer jsonSerializer,
             IKeyPairFactory keyPairFactory,
-            Core.Abstractions.IBolService coreBolService,
+            IBolService bolService,
             IAddressTransformer addressTransformer)
         {
-            _bolService = bolService ?? throw new ArgumentNullException(nameof(bolService));
             _walletService = walletService ?? throw new ArgumentNullException(nameof(walletService));
             _exportKeyFactory = exportKeyFactory ?? throw new ArgumentNullException(nameof(exportKeyFactory));
             _jsonSerializer = jsonSerializer ?? throw new ArgumentNullException(nameof(jsonSerializer));
             _keyPairFactory = keyPairFactory ?? throw new ArgumentNullException(nameof(keyPairFactory));
-            _coreBolService = coreBolService ?? throw new ArgumentNullException(nameof(coreBolService));
+            _bolService = bolService ?? throw new ArgumentNullException(nameof(bolService));
             _addressTransformer = addressTransformer ?? throw new ArgumentNullException(nameof(addressTransformer));
         }
 
         [HttpPost("wallet-individual")]
-        public async Task<ActionResult> CreateWalletB([FromBody] CreateWalletRequest request, CancellationToken token = default)
+        public async Task<ActionResult> CreateWalletB([FromBody] CreateWalletRequest request,
+            CancellationToken token = default)
         {
-            var result = await _walletService.CreateWalletB(request.WalletPassword, request.CodeName, request.Edi, request.PrivateKey, token);
+            var result = await _walletService.CreateWalletB(request.WalletPassword, request.CodeName, request.Edi,
+                request.PrivateKey, token);
 
             return Ok(result);
         }
 
         [HttpPost("wallet-company")]
-        public async Task<ActionResult> CreateWalletC([FromBody] CreateWalletRequest request, CancellationToken token = default)
+        public async Task<ActionResult> CreateWalletC([FromBody] CreateWalletRequest request,
+            CancellationToken token = default)
         {
-            var result = await _walletService.CreateWalletC(request.WalletPassword, request.CodeName, request.Edi, request.PrivateKey, token);
+            var result = await _walletService.CreateWalletC(request.WalletPassword, request.CodeName, request.Edi,
+                request.PrivateKey, token);
 
             return Ok(result);
         }
@@ -64,124 +66,116 @@ namespace Bol.Api.Controllers
         [HttpPost("register")]
         public async Task<ActionResult> Register(CancellationToken token)
         {
-            var result = await _coreBolService.Register(token);
+            var result = await _bolService.Register(token);
+            return Ok(result);
+        }
+
+        [HttpPost("registerAsCertifier")]
+        public async Task<ActionResult> RegisterAsCertifier(IEnumerable<string> countries, string fee,
+            CancellationToken token)
+        {
+            var result = await _bolService.RegisterAsCertifier(countries.Select(c => new Country { Alpha3 = c }),
+                BigInteger.Parse(fee), token);
+            return Ok(result);
+        }
+
+        [HttpPost("unregisterAsCertifier")]
+        public async Task<ActionResult> UnregisterAsCertifier(CancellationToken token)
+        {
+            var result = await _bolService.UnRegisterAsCertifier(token);
             return Ok(result);
         }
 
         [HttpPost("claim")]
         public async Task<ActionResult> Claim(CancellationToken token)
         {
-            var result = await _coreBolService.Claim(token);
+            var result = await _bolService.Claim(token);
             return Ok(result);
         }
 
         [HttpGet("getAccount")]
         public async Task<ActionResult> GetAccount(string codeName, CancellationToken token)
         {
-            var result = await _coreBolService.GetAccount(codeName, token);
+            var result = await _bolService.GetAccount(codeName, token);
             return Ok(result);
         }
 
         [HttpPost("transferClaim")]
         public async Task<ActionResult> TransferClaim(string address, string value, CancellationToken token)
         {
-            var result = await _coreBolService.TransferClaim(_addressTransformer.ToScriptHash(address), BigInteger.Parse(value), token);
+            var result = await _bolService.TransferClaim(_addressTransformer.ToScriptHash(address),
+                BigInteger.Parse(value), token);
             return Ok(result);
         }
 
         [HttpPost("transfer")]
-        public async Task<ActionResult> Transfer(string from, string to, string codeName, string value, CancellationToken token)
+        public async Task<ActionResult> Transfer(string from, string to, string codeName, string value,
+            CancellationToken token)
         {
-            var result = await _coreBolService.Transfer(_addressTransformer.ToScriptHash(from), _addressTransformer.ToScriptHash(to), codeName, BigInteger.Parse(value), token);
+            var result = await _bolService.Transfer(_addressTransformer.ToScriptHash(from),
+                _addressTransformer.ToScriptHash(to), codeName, BigInteger.Parse(value), token);
             return Ok(result);
         }
 
         [HttpPost("whitelist")]
         public async Task<ActionResult> Whitelist(string address, CancellationToken token)
         {
-            var result = await _coreBolService.Whitelist(_addressTransformer.ToScriptHash(address), token);
+            var result = await _bolService.Whitelist(_addressTransformer.ToScriptHash(address), token);
             return Ok(result);
         }
 
         [HttpPost("selectMandatoryCertifiers")]
         public async Task<ActionResult> SelectMandatoryCertifiers(CancellationToken token)
         {
-            var result = await _coreBolService.SelectMandatoryCertifiers(token);
+            var result = await _bolService.SelectMandatoryCertifiers(token);
             return Ok(result);
         }
 
         [HttpPost("payCertificationFees")]
         public async Task<ActionResult> PayCertificationFees(CancellationToken token)
         {
-            var result = await _coreBolService.PayCertificationFees(token);
+            var result = await _bolService.PayCertificationFees(token);
             return Ok(result);
         }
 
         [HttpPost("requestCertification")]
         public async Task<ActionResult> RequestCertification(string codeName, CancellationToken token)
         {
-            var result = await _coreBolService.RequestCertification(codeName, token);
+            var result = await _bolService.RequestCertification(codeName, token);
             return Ok(result);
         }
 
         [HttpPost("certify")]
         public async Task<ActionResult> Certify(string codeName, CancellationToken token)
         {
-            var result = await _coreBolService.Certify(codeName, token);
+            var result = await _bolService.Certify(codeName, token);
             return Ok(result);
         }
 
         [HttpGet("whitelist")]
         public async Task<ActionResult> IsWhitelisted(string address, CancellationToken token)
         {
-            var result = await _coreBolService.IsWhitelisted(_addressTransformer.ToScriptHash(address), token);
-            return Ok(result);
-        }
-
-        [HttpGet("decimals")]
-        public ActionResult Decimals()
-        {
-            var result = _bolService.Decimals();
-            return Ok(result);
-        }
-
-        [HttpGet("name")]
-        public ActionResult Name()
-        {
-            var result = _bolService.Name();
-            return Ok(result);
-        }
-
-        [HttpGet("balanceOf")]
-        public ActionResult BalanceOf()
-        {
-            var result = _bolService.BalanceOf();
-            return Ok(result);
-        }
-
-        [HttpGet("totalSupply")]
-        public ActionResult TotalSupply()
-        {
-            var result = _bolService.TotalSupply();
+            var result = await _bolService.IsWhitelisted(_addressTransformer.ToScriptHash(address), token);
             return Ok(result);
         }
 
         [HttpPost("multi-citizenship")]
         public async Task<ActionResult> AddMultiCitizenship(string shortHash, CancellationToken token)
         {
-            var result = await _coreBolService.AddMultiCitizenship(shortHash, token);
+            var result = await _bolService.AddMultiCitizenship(shortHash, token);
             return Ok(result);
         }
 
         [HttpGet("multi-citizenship")]
         public async Task<ActionResult> IsMultiCitizenship(string shortHash, CancellationToken token)
         {
-            var result = await _coreBolService.IsMultiCitizenship(shortHash, token);
+            var result = await _bolService.IsMultiCitizenship(shortHash, token);
             return Ok(result);
         }
-        
+
         [HttpPost("migrate")]
-        public async Task<ActionResult> Migrate(string currentBolHash, string walletFolderPath, string password, CancellationToken token)
+        public async Task<ActionResult> Migrate(string currentBolHash, string walletFolderPath, string password,
+            CancellationToken token)
         {
             var keys = Directory.GetFiles(walletFolderPath, "*.json")
                 .Select(System.IO.File.ReadAllText)
@@ -212,7 +206,7 @@ namespace Bol.Api.Controllers
                 NewScript = script,
                 CurrentScriptHash = currentBolHash
             };
-            var result = await _coreBolService.MigrateContract(migration, keys, token);
+            var result = await _bolService.MigrateContract(migration, keys, token);
             return Ok(result);
         }
     }
