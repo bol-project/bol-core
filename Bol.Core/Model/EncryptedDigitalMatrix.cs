031061ad8c045f78458c8aff7854b6ef8095a624
Charalampos Chomenidis <hampos@me.com>
2024-01-12 23:58:51 +0200

    Add multi citizenship support to EDI models

--- Bol.Core/Model/EncryptedDigitalMatrix.cs
+++ Bol.Core/Model/EncryptedDigitalMatrix.cs
@@ -1,22 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Bol.Core.Model
 {
+    /// <summary>
+    /// 1st level Matrix used for identification.
+    /// </summary>
     public class EncryptedDigitalMatrix
     {
         public const string CURRENT_VERSION = "1.0";
-
+        
+        /// <summary>
+        /// EDM document format version.
+        /// </summary>
         public string Version { get; set; } = CURRENT_VERSION;
-
+        
+        /// <summary>
+        /// Bol Platform CodeName.
+        /// </summary>
         public string CodeName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string FirstName { get; set; }
-        public string Nin { get; set; }
+        
+        /// <summary>
+        /// Hashes of documents that prove citizenship used in Bol Platform.
+        /// </summary>
+        public CitizenshipHashTable CitizenshipHashes { get; set; }
+        
+        /// <summary>
+        /// Hashes of generic identity documents and other files.
+        /// </summary>
+        public GenericHashTable GenericHashes { get; set; }
+        
+        /// <summary>
+        /// Hashes of Encrypted Citizenship matrices
+        /// </summary>
+        public string[] Citizenships { get; set; }
+    }
 
+    /// <summary>
+    /// 2nd level Matrix used for certification.
+    /// </summary>
+    public class EncryptedDigitalCitizenshipMatrix : EncryptedDigitalMatrix
+    {
+        public EncryptedCitizenship[] CitizenshipMatrices { get; set; }
+    }
+    
+    /// <summary>
+    /// Data and hashes related to a specific citizenship.
+    /// </summary>
+    public class EncryptedCitizenship
+    {
+        public string CountryCode { get; set; }
         public string BirthCountryCode { get; set; }
-        public IEnumerable<string> CountryCodes { get; set; }
-
-        public HashTable Hashes { get; set; }
+        public string Nin { get; set; }
+        public string SurName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+        public CitizenshipHashTable CitizenshipHashes { get; set; }
     }
 }
--- Bol.Core/Model/HashTable.cs
+++ Bol.Core/Model/HashTable.cs
@@ -1,16 +1,50 @@
 ﻿namespace Bol.Core.Model
 {
-    public class HashTable
+    public class CitizenshipHashTable
     {
+        /// <summary>
+        /// National Identity card.
+        /// </summary>
         public string IdentityCard { get; set; }
+        /// <summary>
+        /// Passport.
+        /// </summary>
         public string Passport { get; set; }
+        /// <summary>
+        /// Document that proves ownership of the National Identification Number.
+        /// </summary>
+        public string ProofOfNin { get; set; }
+    }
+    
+    public class GenericHashTable
+    {
+        /// <summary>
+        /// Driving License card.
+        /// </summary>
         public string DrivingLicense { get; set; }
-        public string Photo { get; set; }
-        public string Voice { get; set; }
-        public string NinCertificate { get; set; }
+        /// <summary>
+        /// Document that proves birth.
+        /// </summary>
         public string BirthCertificate { get; set; }
-        public string TelephoneBill { get; set; }
-        public string TextInfo { get; set; }
-        public string Other { get; set; }
+        /// <summary>
+        /// Other forms of identification such as Student Id Card, Military Id Card, Lawyer Id Card.
+        /// </summary>
+        public string OtherIdentity { get; set; }
+        /// <summary>
+        /// A clear passport style photo of person's face.
+        /// </summary>
+        public string FacePhoto { get; set; }
+        /// <summary>
+        /// An audio clip that features the person's voice, stating their name and birthdate.
+        /// </summary>
+        public string PersonalVoice { get; set; }
+        /// <summary>
+        /// A telephone number, email, or social network id, owned by the person.
+        /// </summary>
+        public string ProofOfCommunication { get; set; }
+        /// <summary>
+        /// A document that proves residence such as Power, Telephone or other Utility Bill.
+        /// </summary>
+        public string ProofOfResidence { get; set; }
     }
 }
