f5187922628f063af2f712cf586628a933f85576
Charalampos Chomenidis <hampos@me.com>
2024-01-18 16:17:37 +0200

    Add EDI service tests

---
+++ Bol.Core.Tests/Services/EdiServiceTest.cs
@@ -0,0 +1,218 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bol.Core.Helpers;
+using Bol.Core.Model;
+using Bol.Core.Serializers;
+using Bol.Core.Services;
+using Bol.Core.Validators;
+using Bol.Cryptography.Encoders;
+using Bol.Cryptography.Hashers;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+using Xunit;
+using YamlDotNet.Serialization;
+
+namespace Bol.Core.Tests.Services;
+
+public class EdiServiceTest
+{
+    private readonly EncryptedDigitalIdentityService _ediService;
+
+    public EdiServiceTest()
+    {
+        var hex = new Base16Encoder(new Sha256Hasher());
+        var sha256 = new Sha256Hasher();
+        var ser = new Serializer();
+        var serializer = new YamlSerializer(ser, new Deserializer());
+        var regex = new RegexHelper();
+        var countries = Options.Create(new List<Country>() { new Country() { Alpha3 = "GRC" }, new Country() { Alpha3 = "USA" } });
+        var ninspecs = Options.Create(new List<NinSpecification>(){new NinSpecification{CountryCode = "GRC", Digits = 11}, new NinSpecification{CountryCode = "USA", Digits = 10}});
+        var ccService = new CountryCodeService(countries);
+        var codepValidator = new CodenamePersonValidator(new BasePersonValidator(ccService));
+        var codenameValidator = new CodeNameValidator(codepValidator, new PersonStringSerializer(), sha256, hex);
+        var edmValidator = new EncryptedDigitalMatrixValidator(regex, codenameValidator, new CitizenshipHashTableValidator(regex), new GenericHashTableValidator(regex));
+        var ninService = new NinService(ninspecs);
+        var ecValidator = new EncryptedCitizenshipValidator(regex, ccService, ninService, new CitizenshipHashTableValidator(regex));
+        var eedmValidator = new ExtendedEncryptedDigitalMatrixValidator(edmValidator, ecValidator);
+        var edmcValidator = new EncryptedDigitalMatrixCompanyValidator(regex, codenameValidator, new CompanyHashTableValidator(regex));
+        var eedmcValidator = new ExtendedEncryptedDigitalMatrixCompanyValidator(edmcValidator, new CompanyIncorporationValidator(regex, ccService, ninService, new CitizenshipHashTableValidator(regex)));
+        _ediService = new EncryptedDigitalIdentityService(edmValidator, serializer, sha256, hex, edmcValidator,
+            eedmValidator, eedmcValidator);
+    }
+
+    [Fact]
+    public void GenerateMatrix_ShouldCreateValidEDM()
+    {
+        var eedm = new ExtendedEncryptedDigitalMatrix
+        {
+            CodeName = "P<GRC<PAPA8<G<<<1963M<h1e8C8E7NKM<19B3E",
+            Hashes = new GenericHashTable
+            {
+                IdentityCard = hash("1"),
+                Passport = hash("2"),
+                ProofOfNin = hash("3"),
+                BirthCertificate = hash("4"),
+                DrivingLicense = hash("5"),
+                FacePhoto = hash("6"),
+                ProofOfResidence = hash("7")
+            },
+            CitizenshipMatrices = new[]
+            {
+                new EncryptedCitizenship
+                {
+                    CountryCode = "GRC",
+                    BirthCountryCode = "GRC",
+                    FirstName = "GIANNIS",
+                    SurName = "PAPADOPOULOS",
+                    Nin = "23066301512",
+                    BirthDate = new DateTime(1963, 06, 23),
+                    CitizenshipHashes = new CitizenshipHashTable
+                    {
+                        IdentityCard = hash("1"), 
+                        Passport = hash("2"), ProofOfNin = hash("3"),
+                        BirthCertificate = hash("4")
+                    }
+                },
+                new EncryptedCitizenship
+                {
+                    CountryCode = "USA",
+                    BirthCountryCode = "GRC",
+                    FirstName = "GIANNIS",
+                    SurName = "PAPADOPOULOS",
+                    Nin = "2222222222",
+                    BirthDate = new DateTime(1963, 06, 23),
+                    CitizenshipHashes = new CitizenshipHashTable
+                    {
+                        IdentityCard = hash("11"), Passport = hash("12"), ProofOfNin = hash("13")
+                    }
+                }
+            }
+        };
+
+        var edm = _ediService.GenerateMatrix(eedm);
+        Assert.NotEmpty(edm.Citizenships);
+        Assert.True(edm.Citizenships.Length == eedm.CitizenshipMatrices.Length);
+    }
+
+    [Fact]
+    public void GenerateMatrix_ShouldCreateValidEDMC()
+    {
+        var eedmc = new ExtendedEncryptedDigitalMatrixCompany
+        {
+            CodeName = "C<GRC<ETH4<MET5<POL8<<1914S<Mu61ehzQixw<1B741",
+            Hashes = new CompanyHashTable
+            {
+                ChambersRecords = hash("1"),
+                RegisterOfShareholders = hash("2"),
+                ProofOfVatNumber = hash("3"),
+                ProofOfAddress = hash("4"),
+                IncorporationCertificate = hash("5"),
+                MemorandumAndArticlesOfAssociation = hash("6"),
+                RepresentationCertificate = hash("7"),
+                TaxRegistrationCertificate = hash("8")
+            },
+            Incorporation = new CompanyIncorporation
+            {
+                Title = "ETHNIKO METSOBIO POLYTEXNEIO",
+                VatNumber = "111111111111",
+                IncorporationDate = new DateTime(1914,9,1, 0,0,0, DateTimeKind.Utc),
+            }
+        };
+        var edmc = _ediService.GenerateMatrix(eedmc);
+        
+        Assert.NotEmpty(edmc.IncorporationHash);
+    }
+
+    [Fact]
+    public void GenerateMatrix_ShouldThrowError_WhenExtendedMatrixHasIncorporationHash()
+    {
+        var eedmc = new ExtendedEncryptedDigitalMatrixCompany
+        {
+            CodeName = "C<GRC<ETH4<MET5<POL8<<1914S<Mu61ehzQixw<1B741",
+            Hashes = new CompanyHashTable
+            {
+                ChambersRecords = hash("1"),
+                RegisterOfShareholders = hash("2"),
+                ProofOfVatNumber = hash("3"),
+                ProofOfAddress = hash("4"),
+                IncorporationCertificate = hash("5"),
+                MemorandumAndArticlesOfAssociation = hash("6"),
+                RepresentationCertificate = hash("7"),
+                TaxRegistrationCertificate = hash("8")
+            },
+            IncorporationHash = hash("9"),
+            Incorporation = new CompanyIncorporation
+            {
+                Title = "ETHNIKO METSOBIO POLYTEXNEIO",
+                VatNumber = "111111111111",
+                IncorporationDate = new DateTime(1914,9,1, 0,0,0, DateTimeKind.Utc),
+            }
+        };
+        
+        Assert.Throws<ValidationException>(() => _ediService.GenerateMatrix(eedmc));
+    }
+
+    [Fact]
+    public void GenerateMatrix_ShouldThrowError_WhenExtendedMatrixHasCitizenshipHashes()
+    {
+        var eedm = new ExtendedEncryptedDigitalMatrix
+        {
+            CodeName = "P<GRC<PAPA8<G<<<1963M<h1e8C8E7NKM<19B3E",
+            Hashes =
+                new GenericHashTable
+                {
+                    IdentityCard = hash("1"),
+                    Passport = hash("2"),
+                    ProofOfNin = hash("3"),
+                    BirthCertificate = hash("4"),
+                    DrivingLicense = hash("5"),
+                    FacePhoto = hash("6"),
+                    ProofOfResidence = hash("7")
+                },
+            Citizenships = new []{hash("8")},
+            CitizenshipMatrices = new[]
+            {
+                new EncryptedCitizenship
+                {
+                    CountryCode = "GRC",
+                    BirthCountryCode = "GRC",
+                    FirstName = "GIANNIS",
+                    SurName = "PAPADOPOULOS",
+                    Nin = "23066301512",
+                    BirthDate = new DateTime(1963, 06, 23),
+                    CitizenshipHashes =
+                        new CitizenshipHashTable
+                        {
+                            IdentityCard = hash("1"),
+                            Passport = hash("2"),
+                            ProofOfNin = hash("3"),
+                            BirthCertificate = hash("4")
+                        }
+                },
+                new EncryptedCitizenship
+                {
+                    CountryCode = "USA",
+                    BirthCountryCode = "GRC",
+                    FirstName = "GIANNIS",
+                    SurName = "PAPADOPOULOS",
+                    Nin = "2222222222",
+                    BirthDate = new DateTime(1963, 06, 23),
+                    CitizenshipHashes = new CitizenshipHashTable
+                    {
+                        IdentityCard = hash("11"), Passport = hash("12"), ProofOfNin = hash("13")
+                    }
+                }
+            }
+        };
+
+        Assert.Throws<ValidationException>(() => _ediService.GenerateMatrix(eedm));
+    }
+
+    private string hash(string input)
+    {
+        var hex = new Base16Encoder(new Sha256Hasher());
+        var sha256 = new Sha256Hasher();
+        return hex.Encode(sha256.Hash(Encoding.ASCII.GetBytes(input)));
+    }
+}
