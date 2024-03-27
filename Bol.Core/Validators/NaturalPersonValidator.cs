47dcf0a3203b7942e8332586f0b199770fe3f055
Charalampos Chomenidis <hampos@me.com>
2024-03-27 21:55:48 +0200

    Change Nin validation to accept upper case letters or numbers

--- Bol.Core/Validators/NaturalPersonValidator.cs
+++ Bol.Core/Validators/NaturalPersonValidator.cs
@@ -1,7 +1,6 @@
 using System;
 using Bol.Core.Model;
 using FluentValidation;
-using System.Text.RegularExpressions;
 using Bol.Core.Abstractions;
 
 namespace Bol.Core.Validators
@@ -11,10 +10,7 @@ namespace Bol.Core.Validators
         private readonly IValidator<BasePerson> _basePersonValidator;
         private const int NIN_DIGITS = 11;
 
-        private readonly Regex _capitalLetters = new Regex(@"^[A-Z]+$");
-        private readonly Regex _hexRepresentation = new Regex(@"^[A-F0-9]+$");
-
-        public NaturalPersonValidator(IValidator<BasePerson> basePersonValidator, INinService ninService)
+        public NaturalPersonValidator(IValidator<BasePerson> basePersonValidator, INinService ninService, IRegexHelper regexHelper)
         {
             _basePersonValidator = basePersonValidator ?? throw new ArgumentNullException(nameof(basePersonValidator));
 
@@ -29,7 +25,7 @@ namespace Bol.Core.Validators
                 .WithMessage("National Identification Number cannot be empty.")
                 .Length(p => ninService.GetLength(p.CountryCode))
                 .WithMessage(p => $"National Identification Number (NIN) does not match length for country {p.CountryCode}.")
-                .Must(IsHexRepresentation)
+                .Must(regexHelper.HasAllLettersCapitalOrNumbers)
                 .WithMessage("Nin must be a Base16 (Hex) representation of the SHA256 Hash of the person's National Identification Number.");
 
             RuleFor(p => p.FirstName)
@@ -38,7 +34,7 @@ namespace Bol.Core.Validators
                 .WithMessage("FirstName cannot be empty.")
                 .Length(2, 30)
                 .WithMessage("FirsName cannot be more that 30 characters")
-                .Must(HasAllLettersCapital)
+                .Must(regexHelper.HasAllLettersCapital)
                 .WithMessage("FirstName must consist of capital letters A-Z.");
 
             RuleFor(p => p.Birthdate)
@@ -46,16 +42,6 @@ namespace Bol.Core.Validators
                 .WithMessage("Date of birth must be a valid date.")
                 .InclusiveBetween(DateTime.UtcNow.AddYears(-130), DateTime.UtcNow)
                 .WithMessage("Date of birth cannot be greater than current date and less than 130 years ago");
-        }
-
-        private bool HasAllLettersCapital(string input)
-        {
-            return _capitalLetters.IsMatch(input);
-        }
-
-        private bool IsHexRepresentation(string input)
-        {
-            return _hexRepresentation.IsMatch(input);
         }
     }
 }
